caaf94ce3e39409441f69c011e15c1913599d0a4
Dries Kimpe <dkimpe@mcs.anl.gov>
2008-12-26 19:54:28 -0600

    Reorganization of source tree
    * Made 2 implementations of zoidfs client side API
    (with conflicting symbol names)
    * Create fuse-zoid bridge and two applications
    (one for each zoidfs api implementation)

---
+++ src/zoidfs-posix/zoidfs-template.c
@@ -0,0 +1,215 @@
+
+/*
+ * zoidfs_null
+ * This function implements a noop operation. The IOD returns a 1-byte message
+ * to the CN.
+ */
+void zoidfs_null(void) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_getattr
+ * This function retrieves the attributes associated with the file handle from
+ * the ION.
+ */
+int zoidfs_getattr(const zoidfs_handle_t *handle, zoidfs_attr_t *attr) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_setattr
+ * This function sets the attributes associated with the file handle.
+ */
+int zoidfs_setattr(const zoidfs_handle_t *handle, const zoidfs_sattr_t *sattr,
+                   zoidfs_attr_t *attr) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_readlink
+ * This function reads a symbolic link.
+ */
+int zoidfs_readlink(const zoidfs_handle_t *handle, char *buffer,
+                    size_t buffer_length) {
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_lookup
+ * This function returns the file handle associated with the given file or
+ * directory name.
+ */
+int zoidfs_lookup(const zoidfs_handle_t *parent_handle,
+                  const char *component_name, const char *full_path,
+                  zoidfs_handle_t *handle) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_remove
+ * This function removes the given file or directory.
+ */
+int zoidfs_remove(const zoidfs_handle_t *parent_handle,
+                  const char *component_name, const char *full_path,
+                  zoidfs_cache_hint_t *parent_hint) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_commit
+ * This function flushes the buffers associated with the file handle.
+ */
+int zoidfs_commit(const zoidfs_handle_t *handle) 
+{
+    return -ENOSYS;
+
+}
+
+
+/*
+ * zoidfs_create
+ * This function creates a new file.
+ */
+int zoidfs_create(const zoidfs_handle_t *parent_handle,
+                  const char *component_name, const char *full_path,
+                  const zoidfs_sattr_t *sattr, zoidfs_handle_t *handle,
+                  int *created) 
+{
+
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_rename
+ * This function renames an existing file or directory.
+ */
+int zoidfs_rename(const zoidfs_handle_t *from_parent_handle,
+                  const char *from_component_name,
+                  const char *from_full_path,
+                  const zoidfs_handle_t *to_parent_handle,
+                  const char *to_component_name,
+                  const char *to_full_path,
+                  zoidfs_cache_hint_t *from_parent_hint,
+                  zoidfs_cache_hint_t *to_parent_hint) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_symlink
+ * This function creates a symbolic link.
+ */
+int zoidfs_symlink(const zoidfs_handle_t *from_parent_handle,
+                   const char *from_component_name,
+                   const char *from_full_path,
+                   const zoidfs_handle_t *to_parent_handle,
+                   const char *to_component_name,
+                   const char *to_full_path, const zoidfs_sattr_t *sattr,
+                   zoidfs_cache_hint_t *from_parent_hint,
+                   zoidfs_cache_hint_t *to_parent_hint) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_mkdir
+ * This function creates a new directory.
+ */
+int zoidfs_mkdir(const zoidfs_handle_t *parent_handle,
+                 const char *component_name, const char *full_path,
+                 const zoidfs_sattr_t *sattr,
+                 zoidfs_cache_hint_t *parent_hint) 
+{
+
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_readdir
+ * This function returns the dirents from the specified parent directory. The
+ * cookie is a pointer which specifies where in the directory to start
+ * fetching the dirents from.
+ */
+int zoidfs_readdir(const zoidfs_handle_t *parent_handle,
+                   zoidfs_dirent_cookie_t cookie, int *entry_count,
+                   zoidfs_dirent_t *entries,
+                   zoidfs_cache_hint_t *parent_hint) 
+{
+    return -ENOSYS;
+
+}
+
+
+/*
+ * zoidfs_resize
+ * This function resizes the file associated with the file handle.
+ */
+int zoidfs_resize(const zoidfs_handle_t *handle, uint64_t size) 
+{
+
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_write
+ * This function implements the zoidfs write call.
+ */
+int zoidfs_write(const zoidfs_handle_t *handle, int mem_count,
+                 const void *mem_starts[], const size_t mem_sizes[],
+                 int file_count, const uint64_t file_starts[],
+                 uint64_t file_sizes[]) 
+{
+
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_read
+ * This function implements the zoidfs read call.
+ */
+int zoidfs_read(const zoidfs_handle_t *handle, int mem_count,
+                void *mem_starts[], const size_t mem_sizes[], int file_count,
+                const uint64_t file_starts[], uint64_t file_sizes[]) 
+{
+
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_init
+ * Initialize the client subsystems.
+ */
+int zoidfs_init(void) 
+{
+    return -ENOSYS;
+}
+
+
+/*
+ * zoidfs_finalize
+ * Finalize the client subsystems.
+ */
+int zoidfs_finalize(void) 
+{
+    return -ENOSYS;
+}
+
