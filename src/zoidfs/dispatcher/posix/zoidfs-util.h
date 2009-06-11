4a0501bf7414f6b106c152ca2404b2e15974cade
kzk <kzk@pfitera.(none)>
2009-06-12 05:18:56 +0900

    * now build posix-dispatcher layer
    TODO:
      - more proper configure.ac
      - remove duplication of zoidfs-util.[ch] in fuse directory

---
+++ src/zoidfs/dispatcher/posix/zoidfs-util.h
@@ -0,0 +1,27 @@
+#ifndef ZOIDFS_UTIL_H
+#define ZOIDFS_UTIL_H
+
+#include <string.h>
+#include "zoidfs/zoidfs.h"
+
+/* Convert zoidfs handle into human readable string */ 
+int zoidfs_handle_to_text (const zoidfs_handle_t * handle, char * buf, int
+      bufsize); 
+
+/* Convert human readable zoidfs handle into handle */ 
+int zoidfs_text_to_handle (const char * buf, zoidfs_handle_t * handle); 
+
+/* Return pointer to string repr of handle */ 
+const char * zoidfs_handle_string (const zoidfs_handle_t * handle);
+
+
+/* Return 1 if the two handles are equal */
+static inline int zoidfs_handle_equal (const zoidfs_handle_t * h1, const
+      zoidfs_handle_t * h2)
+{
+   return (memcmp (h1, h2, sizeof(zoidfs_handle_t))==0); 
+}
+
+
+
+#endif
