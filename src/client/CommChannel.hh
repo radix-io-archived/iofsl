5f059d866ba992f19aa2cebfe9ac9614341a3578
kzk <kzk@pfitera.(none)>
2009-06-17 05:55:10 +0900

    * add client-side write() code   - NOTICE: not compilable

--- src/client/CommChannel.hh
+++ src/client/CommChannel.hh
@@ -78,6 +78,44 @@ namespace client
             return request_reader_; 
          }
 
+         template<typename SENDREQ, typename RECEIVEREQ>
+         int beforeExecuteOp(int opid, const SENDREQ & send, const RECEIVEREQ & recv)
+         {
+            int ret;
+            // Reset XDR streams and indicate operation code
+            prepareOp (opid); 
+
+            // Make sure the send buffer is large enough 
+            applyTypes (prepareParam(), send); 
+
+            // Init XDR encoder and serialize operation ID
+            sendParamInit (); 
+
+            // Serialize the request parameters
+            applyTypes (sendParamCont(), send); 
+
+            // All generic operations include a status code as part of the
+            // reply
+            prepareReply () << ret; 
+
+            // Ensure the buffer for the server's answer is large enough
+            applyTypes (prepareReply(), recv); 
+         }
+
+         template<typename SENDREQ, typename RECEIVEREQ>
+         int afterExecuteOp(int opid, const SENDREQ & send, const RECEIVEREQ & recv)
+         {
+            int ret;
+
+            // All generic operations return a status code first 
+            getReply () >> ret;
+
+            // Get the rest of the reply 
+            applyTypes (getReply (), recv);
+            
+            return ret;
+         }
+
          /// Execute the request
          void execute ()
          {
@@ -98,42 +136,57 @@ namespace client
             request_reader_.reset (buffer_receive_.get (needed), needed); 
          }
 
-
          // Generic operation suitable for most operations
          template <typename SENDREQ, typename RECEIVEREQ>
          int genericOp (int opid, const SENDREQ & send, const RECEIVEREQ & recv)
          {
             int ret; 
-
-            // Reset XDR streams and indicate operation code
-            prepareOp (opid); 
-
-            // Make sure the send buffer is large enough 
-            applyTypes (prepareParam(), send); 
-
-            // Init XDR encoder and serialize operation ID
-            sendParamInit (); 
-
-            // Serialize the request parameters
-            applyTypes (sendParamCont(), send); 
-
-            // All generic operations include a status code as part of the
-            // reply
-            prepareReply () << ret; 
-
-            // Ensure the buffer for the server's answer is large enough
-            applyTypes (prepareReply(), recv); 
-
+            beforeExecuteOp (opid, send, recv);
             // Execute the send and wait for receive
             execute (); 
-
-            // All generic operations return a status code first 
-            getReply () >> ret; 
-
-            // Get the rest of the reply 
-            applyTypes (getReply (), recv); 
-
+            ret = afterExecuteOp (opid, send, recv);
             return ret; 
+         }
+
+         /// Execute the request
+         void executeWriteOp (const void ** buf_list, const size_t * size_list, size_t list_count)
+         {
+           /*
+            // Make sure the receive buffer is large enough for the reply
+            const size_t needed = receiveSizeProcessor_.getSize().actual; 
+            buffer_receive_.resize (needed); 
+            // post receive
+            iofwdutil::bmi::BMIOp receive = bmi_->postReceive (iofwdhost_, 
+                  buffer_receive_.get(), buffer_receive_.size (), 
+                  buffer_receive_.bmiType(), ZOIDFS_REQUEST_TAG);
+            // send unexpected
+            bmi_->postSendUnexpected (iofwdhost_, 
+                  buffer_send_.get(), buffer_send_.size(), 
+                  buffer_send_.bmiType(), ZOIDFS_REQUEST_TAG); 
+            // send data
+            size_t total_size = 0;
+            for (size_t i = 0; i < list_count; i++) total_size += size_list[i];
+            iofwdutil::bmi::BMIOp sendlist = bmi_->postSendList(iofwdhost_, buf_list, size_list, list_count,
+                  total_size, BMI_PRE_ALLOC, ZOIDFS_REQUEST_TAG);
+            // Wait send list
+            size_t UNUSED(sendbytes) = sendlist.wait ();
+            // Wait recv
+            size_t UNUSED(received) = receive.wait (); 
+            // Reset XDR deserialization
+            request_reader_.reset (buffer_receive_.get (needed), needed);
+           */
+         }
+
+         // Write operation
+         template <typename SENDREQ, typename RECEIVEREQ>
+         int writeOp (int opid, const SENDREQ & send, const RECEIVEREQ & recv,
+                      const void ** buf_list, const size_t * size_list, size_t list_count)
+         {
+            int ret;
+            beforeExecuteOp (opid, send, recv);
+            executeWriteOp (buf_list, size_list, list_count);
+            ret = afterExecuteOp (opid, send, recv);
+            return ret;
          }
 
     protected:
