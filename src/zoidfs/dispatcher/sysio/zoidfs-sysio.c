7181d007c7b85331908d8281ee188c2273791ab9
Jason Cope <copej@mcs.anl.gov>
2009-08-02 22:51:56 -0500

    * removed UNUSED(x) macro define and added tools.h

--- src/zoidfs/dispatcher/sysio/zoidfs-sysio.c
+++ src/zoidfs/dispatcher/sysio/zoidfs-sysio.c
@@ -31,15 +31,7 @@
 #include "fhi.h"
 
 #include "zoidfs.h"
-
-#ifdef UNUSED
-#elif defined(__GNUC__)
-# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
-#elif defined(__LCLINT__)
-# define UNUSED(x) /*@unused@*/ x
-#else
-# define UNUSED(x) x
-#endif
+#include "c-util/tools.h"
 
 /*
  * zoidfs sysio init variables
