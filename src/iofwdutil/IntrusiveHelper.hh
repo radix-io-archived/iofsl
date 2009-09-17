d43af6a02342b25db8b448f1c354fc6ec3036ea7
Dries Kimpe <dkimpe@mcs.anl.gov>
2009-09-16 20:29:35 -0500

    Minor updates (code,document) for IntrusiveHelper

--- src/iofwdutil/IntrusiveHelper.hh
+++ src/iofwdutil/IntrusiveHelper.hh
@@ -25,29 +25,47 @@ public:
 
    ~IntrusiveHelper ()
    {
+      /* Make sure nobody is still pointing to us */
+      ASSERT(!refcount_);
    }
 
-   int unref ()
+   int removeref ()
    {
       ASSERT(refcount_); 
       return --refcount_; 
    }
 
-   int ref ()
+   int addref ()
    {
       return ++refcount_; 
    }
 
+   /**
+    * Returns true if there are still shared pointers pointing to us.
+    */
+   bool alive () const
+   {
+      return refcount_;
+   }
 private:
    int refcount_; 
 }; 
 
+
+/* @NOTE:
+ *    * Can simplify this by providing a template version
+ *      of the functions below.
+ *    * This is not thread safe; Can make it thread safe 
+ *      for all versions or just for certain classes 
+ *      by providing another PTR_HELPER macro or specializing
+ *      the template function.
+ */
 #define INTRUSIVE_PTR_HELPER(cls) \
    inline void intrusive_ptr_add_ref(cls * r) \
-   { r->ref(); } \
+   { r->addref(); } \
    \
    inline void intrusive_ptr_release(cls * r)\
-   { if (!r->unref()) { delete r; } }
+   { if (!r->removeref()) { delete r; } }
 
 
 
