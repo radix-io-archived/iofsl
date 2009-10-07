0f50c42e2988aa45cdb9dbdc01dd0c1cd8142611
Dries Kimpe <dkimpe@mcs.anl.gov>
2009-10-07 13:51:00 -0500

    White-space fix+extra assertions in handle2string

--- src/zoidfs/util/zoidfs-util.cpp
+++ src/zoidfs/util/zoidfs-util.cpp
@@ -1,7 +1,8 @@
 #include <boost/format.hpp>
+#include "iofwdutil/assert.hh"
 #include "zoidfs-util.hh"
 
-using namespace boost; 
+using namespace boost;
 
 
 namespace zoidfs
@@ -11,38 +12,39 @@ namespace zoidfs
    std::string handle2string (const zoidfs_handle_t  * handle)
    {
       if (!handle)
-      { 
-         return std::string("(null)"); 
+      {
+         return std::string("(null)");
       }
-      char buf[sizeof(handle->data)*2+1]; 
-      char * bufptr = buf; 
+      char buf[sizeof(handle->data)*2+1];
+      char * bufptr = buf;
       for (unsigned int i=0; i<sizeof(handle->data); ++i)
       {
-         unsigned char byte = handle->data[i]; 
+         unsigned char byte = handle->data[i];
          for (unsigned int j=0; j<2; ++j)
          {
-            int ret = (byte >> 4) & 0xf; 
+            int ret = (byte >> 4) & 0xf;
             if (ret < 10)
-               *bufptr++ = static_cast<char>('0' + ret); 
+               *bufptr++ = static_cast<char>('0' + ret);
             else
-               *bufptr++ = static_cast<char>('a' + (ret - 10)); 
-            byte = static_cast<char> (byte << 4); 
+               *bufptr++ = static_cast<char>('a' + (ret - 10));
+            byte = static_cast<char> (byte << 4);
          }
       }
-      *bufptr = 0; 
-      return std::string(buf); 
+      *bufptr++ = 0;
+      ASSERT((size_t) (bufptr - &buf[0]) <= sizeof(buf));
+      return std::string(buf);
    }
-   
+
    static inline const char * safestring (const char * s)
    {
-      return (s ? s : "(null)"); 
+      return (s ? s : "(null)");
    }
 
    std::string filespec2string (const zoidfs_handle_t* parent_handle,
          const char * component, const char * full_path)
    {
       return str(format("handle=%s comp=%s full=%s") % handle2string
-            (parent_handle) % safestring(component) % safestring(full_path)); 
+            (parent_handle) % safestring(component) % safestring(full_path));
    }
 
 //===========================================================================
