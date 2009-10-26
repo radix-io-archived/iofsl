520902e79401f0db51f2af66dd41a46e3c837162
Jason Cope <copej@mcs.anl.gov>
2009-10-25 21:34:44 -0500

    renamed state machine wrapper and expanded template parameters.

--- src/sm/IOFWDStateMachine.hh
+++ src/sm/IOFWDStateMachine.hh
@@ -9,6 +9,7 @@
 #include <boost/statechart/event.hpp>
 #include <boost/statechart/result.hpp>
 #include <boost/statechart/null_exception_translator.hpp>
+#include <boost/statechart/exception_translator.hpp>
 
 #include <boost/intrusive_ptr.hpp>
 
@@ -17,36 +18,20 @@
 #include "IOFWDState.hh"
 #include "IOFWDStateEvents.hh"
 
-/*
- * IOFWDStateMachine - the base state machine for IOFWD processes
- */
-template<
-    class StateMachine,
-    class StartState >
-struct IOFWDStateMachine : boost::statechart::asynchronous_state_machine< IOFWDStateMachine< StateMachine, StartState >,
-                                                                            StartState,
-                                                                            boost::statechart::fifo_scheduler< >,
-                                                                            std::allocator< void >,
-                                                                            boost::statechart::null_exception_translator >
-{
-    public:
-        virtual void run(); 
-};
-
 /* IOFWD State Machine interface */
 template< class IOFWDStateMachineType >
-struct IOFWDStateMachineManager
+struct IOFWDStateMachine
 {
     public:
         /* create the event scheduler and processor */
-        IOFWDStateMachineManager() : scheduler_( true )
+        IOFWDStateMachine(bool trace) : scheduler_( true ), trace_(trace)
         {
             ph_ = scheduler_.create_processor< IOFWDStateMachineType >();
             scheduler_.initiate_processor(ph_);
         }
 
         /* Terminate the scheduler and event processor */
-        ~IOFWDStateMachineManager()
+        ~IOFWDStateMachine()
         {
             scheduler_.destroy_processor(ph_);
             scheduler_.terminate();
@@ -60,12 +45,35 @@ struct IOFWDStateMachineManager
 
         void queue_event(IOFWDEvent * iofwdEvent)
         {
+            iofwdEvent->enableTrace(trace_);
             scheduler_.queue_event(ph_, boost::intrusive_ptr< const boost::statechart::event_base >(iofwdEvent));
         }
 
     protected:
         boost::statechart::fifo_scheduler<> scheduler_;
         boost::statechart::fifo_scheduler<>::processor_handle ph_;
+    
+        bool trace_;
+};
+
+/*
+ * IOFWDStateMachine - the base state machine for IOFWD processes
+ */
+template<
+    class StateMachine,
+    class StartState,
+    class StateScheduler = boost::statechart::fifo_scheduler< >,
+    class StateAllocator = std::allocator< void >,
+    class StateExceptionTrans = boost::statechart::exception_translator<>
+     >
+struct IOFWDAsyncStateMachine : boost::statechart::asynchronous_state_machine< IOFWDAsyncStateMachine< StateMachine, StartState, StateScheduler, StateAllocator, StateExceptionTrans>,
+                                                                            StartState,
+                                                                            StateScheduler,
+                                                                            StateAllocator,
+                                                                            StateExceptionTrans >
+{
+    public:
+        virtual void run(); 
 };
 
 #endif
