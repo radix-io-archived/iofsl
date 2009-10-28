f78f3adc9377a30e1414002b5ada04e041ed04d9
Dries Kimpe <dkimpe@mcs.anl.gov>
2009-10-28 14:15:15 -0500

    BMI Resource complete (untested)

--- src/iofwdevent/BMIResource.cpp
+++ src/iofwdevent/BMIResource.cpp
@@ -1,15 +1,9 @@
+#include "iofwdutil/tools.hh"
 #include "BMIResource.hh"
 
 namespace iofwdevent
 {
-
-   void BMIResource::stop ()
-   {
-   }
-
-   void BMIResource::start ()
-   {
-   }
+//===========================================================================
 
    BMIResource::~BMIResource ()
    {
@@ -17,8 +11,57 @@ namespace iofwdevent
    }
 
    BMIResource::BMIResource ()
+      : mempool_ (sizeof(BMIEntry))
    {
       BMI_open_context (&context_);
    }
+   
+   void BMIResource::stop ()
+   {
+      ThreadedResource::stop ();
 
+      // currently there is no way to cancel all BMI requests of a certain
+      // context. 
+   }
+
+   void BMIResource::handleBMIError (ResourceOp * UNUSED(u), int UNUSED(bmiret))
+   {
+      // TODO: convert into exception and call u->exception
+               // TODO: maybe check for cancel?
+               // Shouldn't make a difference right now since we don't do
+               // cancel.
+      ALWAYS_ASSERT(false && "TODO");
+   }
+
+   /**
+    * Worker thread: just calls testcontext over and over.
+    */
+   void BMIResource::threadMain ()
+   {
+      std::vector<bmi_op_id_t> opids_ (CHECK_COUNT);
+      std::vector<bmi_error_code_t> errors_ (CHECK_COUNT);
+      std::vector<BMIEntry *> users_ (CHECK_COUNT);
+      std::vector<bmi_size_t> sizes_ (CHECK_COUNT);
+      
+      while (!needShutdown ())
+      {
+         int outcount;
+         checkBMI (BMI_testcontext (opids_.size(), &opids_[0],
+                  &outcount, &errors_[0], &sizes_[0],
+                  reinterpret_cast<void**>(&users_[0]), WAIT_TIME, context_));
+
+         for (int i=0; i<outcount; ++i)
+         {
+            BMIEntry & e = *users_[i];
+
+            if (e.actual)
+               *e.actual = sizes_[i];
+
+            // completeEntry frees the BMIEntry
+            completeEntry (&e, errors_[i]);
+         }
+      }
+   }
+
+//===========================================================================
 }
--- src/iofwdevent/BMIResource.hh
+++ src/iofwdevent/BMIResource.hh
@@ -6,26 +6,231 @@ extern "C"
 #include <bmi.h>
 }
 
-#include "Resource.hh"
+#include <boost/pool/pool.hpp>
+
+#include "ThreadedResource.hh"
+#include "ResourceOp.hh"
+#include "iofwdutil/assert.hh"
 
 namespace iofwdevent
 {
+   //===========================================================================
 
-   class BMIResource : public Resource
+   class BMIResource : public ThreadedResource
    {
-   public:
+      public:
+      
+         enum {
+            // Number of completed message to handle in one call to
+            // testcontext
+            CHECK_COUNT = 64,
 
-      virtual void stop ();
+            // How long we wait in testcontext
+            WAIT_TIME = 1000
+         };
 
-      virtual void start ();
+         BMIResource ();
 
-      BMIResource ();
+         virtual ~BMIResource ();
 
-      virtual ~BMIResource ();
+         virtual void stop ();
 
-   protected:
-      bmi_context_id context_;
+
+      protected:
+         // Note: don't store objects in here, mempool doesn't
+         // destruct/construct properly
+         struct BMIEntry 
+         {
+            ResourceOp * op;
+            bmi_size_t * actual;
+         };
+
+      protected:
+
+         virtual void threadMain ();
+
+         /**
+          * Check if there was a BMI error; If so, deal with it.
+          * Check if the operation completed immediately. If so, 
+          * call the completion callback right away.
+          */
+         inline void checkBMISendRecv (BMIEntry * e, int bmiret);
+
+         /** 
+          * Call the correct callback and return the entry back to the mempool.
+          */
+         inline void completeEntry (BMIEntry * e, int bmiret);
+
+         /**
+          * Translate BMI error code into exception
+          * (which will be tunneled into the ResourceOp)
+          */
+         void handleBMIError (ResourceOp * u, int bmiret);
+
+         /**
+          * Return new BMI entry 
+          */
+         inline BMIEntry * newEntry (ResourceOp * u, bmi_size_t * actual = 0);
+
+         /**
+          * CHeck for normal BMI errors not associated with requests
+          */
+         inline void checkBMI (int ret) { ALWAYS_ASSERT(ret >= 0); };
+
+      public:
+
+         inline void post_send (ResourceOp * u, BMI_addr_t dest,
+               const void * buffer,
+               bmi_size_t size,
+               enum bmi_buffer_type buffer_type,
+               bmi_msg_tag_t tag,
+               bmi_hint hints);
+
+         inline void post_recv(ResourceOp * u, BMI_addr_t src,
+               void *buffer,
+               bmi_size_t expected_size,
+               bmi_size_t * actual_size,
+               enum bmi_buffer_type buffer_type,
+               bmi_msg_tag_t tag,
+               bmi_hint hints);
+
+         inline void post_send_list(ResourceOp * u, BMI_addr_t dest,
+               const void *const *buffer_list,
+               const bmi_size_t* size_list,
+               int list_count,
+               /* "total_size" is the sum of the size list */
+               bmi_size_t total_size,
+               enum bmi_buffer_type buffer_type,
+               bmi_msg_tag_t tag,
+               bmi_hint hints);
+
+         inline void post_recv_list(ResourceOp * u,
+               BMI_addr_t src,
+               void *const *buffer_list,
+               const bmi_size_t *size_list,
+               int list_count,
+               bmi_size_t total_expected_size,
+               bmi_size_t * total_actual_size,
+               enum bmi_buffer_type buffer_type,
+               bmi_msg_tag_t tag,
+               bmi_hint hints);
+
+      protected:
+         bmi_context_id context_;
+
+         // Note: not object pool, no destructor/constructor will be called
+         boost::pool<> mempool_;
    };
+
+   //===========================================================================
+
+   BMIResource::BMIEntry * BMIResource::newEntry (ResourceOp * u, 
+         bmi_size_t * actual)
+   {
+      BMIEntry * e = (BMIEntry *) mempool_.malloc ();
+      e->op = u;
+      e->actual = actual;
+      return e;
+   }
+
+   void BMIResource::completeEntry (BMIEntry * e, int bmiret)
+   {
+      try
+      {
+         if (bmiret >= 0)
+         {
+            e->op->success ();
+         }
+         else
+         {
+            handleBMIError (e->op, bmiret);
+         }
+      }
+      catch (...)
+      {
+         ALWAYS_ASSERT(false && "ResourceOp should not throw");
+      }
+      mempool_.free (e);
+   }
+
+
+   void BMIResource::checkBMISendRecv (BMIEntry * e, int bmiret)
+   {
+      // If return is 0, the operation is posted.
+      if (!bmiret)
+         return;
+
+      // Return is != 0, so either immediate completion or error.
+      // Either way, we can call the callback now and release the BMIEntry
+      // memory.
+      completeEntry (e, bmiret);
+   }
+
+   void BMIResource::post_send (ResourceOp * u, BMI_addr_t dest,
+         const void * buffer,
+         bmi_size_t size,
+         enum bmi_buffer_type buffer_type,
+         bmi_msg_tag_t tag,
+         bmi_hint hints)
+   {
+      bmi_op_id_t id;
+      BMIEntry * e = newEntry (u);
+      checkBMISendRecv (e, BMI_post_send (&id, dest, buffer, size, buffer_type, tag,
+               e, context_, hints));
+   }
+
+   void BMIResource::post_recv(ResourceOp * u, BMI_addr_t src,
+         void *buffer,
+         bmi_size_t expected_size,
+         bmi_size_t * actual_size,
+         enum bmi_buffer_type buffer_type,
+         bmi_msg_tag_t tag,
+         bmi_hint hints)
+   {
+      bmi_op_id_t op;
+      BMIEntry * e = newEntry (u, actual_size);
+
+      checkBMISendRecv (e, BMI_post_recv (&op, src, buffer, expected_size, 
+               actual_size, buffer_type, tag, e, context_, hints));
+   }
+
+   void BMIResource::post_send_list(ResourceOp * u, BMI_addr_t dest,
+         const void *const *buffer_list,
+         const bmi_size_t* size_list,
+         int list_count,
+         /* "total_size" is the sum of the size list */
+         bmi_size_t total_size,
+         enum bmi_buffer_type buffer_type,
+         bmi_msg_tag_t tag,
+         bmi_hint hints)
+   {
+      bmi_op_id_t op;
+      BMIEntry * e = newEntry (u);
+      checkBMISendRecv (e, BMI_post_send_list (&op,
+               dest, buffer_list, size_list, list_count,
+               total_size, buffer_type, tag, e, context_, hints));
+   }
+
+   void BMIResource::post_recv_list(ResourceOp * u,
+         BMI_addr_t src,
+         void *const *buffer_list,
+         const bmi_size_t *size_list,
+         int list_count,
+         bmi_size_t total_expected_size,
+         bmi_size_t * total_actual_size,
+         enum bmi_buffer_type buffer_type,
+         bmi_msg_tag_t tag,
+         bmi_hint hints)
+   {
+      bmi_op_id_t op;
+      BMIEntry * e = newEntry (u, total_actual_size);
+      checkBMISendRecv (e, BMI_post_recv_list(&op, src, buffer_list,
+               size_list, list_count, total_expected_size, total_actual_size,
+               buffer_type, tag, e, context_, hints));
+   }
+
+
+   //===========================================================================
 }
 
 #endif
