4b3b05c76fbfda434dc7ff7acf5c2cdc9e70c668
Dries Kimpe <dkimpe@mcs.anl.gov>
2009-12-11 16:41:21 -0600

    Boost.Test macro's are not thread safe.
    Added synchronized versions of BOOST_CHECK, BOOST_TEST_MESSAGE, ...
    They synchronize using a global lock.

---
+++ test/boost/ThreadSafety.cpp
@@ -0,0 +1,11 @@
+#include "ThreadSafety.hh"
+
+namespace test
+{
+   namespace boost
+   {
+
+      ::boost::mutex boost_test_lock_;
+
+   }
+}
---
+++ test/boost/ThreadSafety.hh
@@ -0,0 +1,44 @@
+#ifndef TEST_BOOST_THREADSAFETY_HH
+#define TEST_BOOST_THREADSAFETY_HH
+
+/**
+ * Boost.Test is not thread safe.
+ * Define thread safe versions of macro's
+ */
+
+#include <boost/thread.hpp>
+
+namespace test
+{
+   namespace boost
+   {
+
+      extern ::boost::mutex boost_test_lock_;
+
+   }
+}
+
+#define BOOST_CHECK_EQUAL_TS(a,b) \
+ { \
+    boost::mutex::scoped_lock l(::test::boost::boost_test_lock_); \
+    BOOST_CHECK_EQUAL(a,b); }
+
+#define BOOST_CHECK_MESSAGE_TS(a,b) \
+{\
+   boost::mutex::scoped_lock l(::test::boost::boost_test_lock_); \
+   BOOST_CHECK_MESSAGE(a,b); \
+}
+
+#define BOOST_CHECK_TS(a) \
+{\
+   boost::mutex::scoped_lock l(::test::boost::boost_test_lock_);\
+   BOOST_CHECK(a);\
+}\
+
+#define BOOST_TEST_MESSAGE_TS(a) \
+{\
+   boost::mutex::scoped_lock l(::test::boost::boost_test_lock_);\
+   BOOST_TEST_MESSAGE(a);\
+}
+
+#endif
