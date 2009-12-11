da3c2f8d1c179f99f3cf400c0a954bec2a58e4bd
Dries Kimpe <dkimpe@mcs.anl.gov>
2009-12-11 14:22:06 -0600

    Use own singleton implementation.
    boost::call_once has some problems according to valgrind.
    Added own implementation but didn't enable it since the boost one uses
    a pthread optimized algorithm and is likely to be faster.
    In future could enable it by default when using VALGRIND_SAFE

--- src/iofwdutil/Singleton.hh
+++ src/iofwdutil/Singleton.hh
@@ -2,8 +2,15 @@
 #define IOFWDUTIL_SINGLETON_HPP
 
 #include <boost/utility.hpp>
-#include <boost/thread/once.hpp>
 #include <boost/scoped_ptr.hpp>
+
+
+#ifdef USE_OWN_SINGLETON
+#include <csignal>
+#include <boost/thread.hpp>
+#else
+#include <boost/thread/once.hpp>
+#endif
 
 namespace iofwdutil
 {
@@ -12,13 +19,40 @@ namespace iofwdutil
 template<class T>
 class Singleton : private boost::noncopyable
 {
+#ifdef USE_OWN_SINGLETON
+protected:
+   enum { NOT_INITIALIZED = 0,
+          INITIALIZED     = 2 };
+#endif
 
 public:
+
+#ifdef USE_OWN_SINGLETON
+   static void build ()
+   {
+      boost::mutex::scoped_lock l(lock_);
+      // Check again in case more than one thread made it into build.
+      if (status_ != INITIALIZED)
+         return;
+
+      init ();
+      status_ = INITIALIZED;
+   }
+
     static T& instance()
+    {
+       if (status_ != INITIALIZED)
+          build ();
+
+       return *t;
+    }
+#else
+ static T& instance()
     {
         boost::call_once(init, flag);
         return *t;
     }
+#endif
 
     static void init() // never throws
     {
@@ -31,12 +65,25 @@ protected:
 
 private:
      static boost::scoped_ptr<T> t;
-     static boost::once_flag flag;
+
+#ifdef USE_OWN_SINGLETON
+     static sig_atomic_t status_;
+     static boost::mutex lock_;
+#else
+  static boost::once_flag flag;
+#endif
 };
 
 template<class T> boost::scoped_ptr<T> iofwdutil::Singleton<T>::t(0);
 
+#ifdef USE_OWN_SINGLETON
+template<class T> boost::mutex iofwdutil::Singleton<T>::lock_;
+template<class T> sig_atomic_t iofwdutil::Singleton<T>::status_ =
+                           iofwdutil::Singleton<T>::NOT_INITIALIZED;
+#else
 template<class T> boost::once_flag iofwdutil::Singleton<T>::flag = BOOST_ONCE_INIT;
+#endif
+
 
 //===========================================================================
 }
