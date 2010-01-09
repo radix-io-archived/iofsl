e8f48a87c2e5117387c10ea5afcb14a5714b4e28
Dries Kimpe <dkimpe@mcs.anl.gov>
2010-01-08 18:34:31 -0600

    Small enhancement to scripts/prepush.sh

--- scripts/runtest-range.sh
+++ scripts/runtest-range.sh
@@ -6,6 +6,12 @@ if test -z "${IOFWD_SRCDIR}" ; then
 fi
 
 SCRIPTDIR="${IOFWD_SRCDIR}/scripts"
+LOGDIR="${IOFWD_SRCDIR}/buildlogs"
+LOGFILE="${LOGDIR}/runtest-rev.log"
+
+COUNT=$(git log --format=oneline $1 | wc -l)
+
+echo "Starting runtest-range of '$1' (${COUNT} revs)" >> ${LOGFILE}
 
 git log --format=oneline $1 | tac | cut -d' ' -f1 \
         | while read rev
