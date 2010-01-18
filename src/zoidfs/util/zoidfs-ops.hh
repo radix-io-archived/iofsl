773422079d3505632caf38de1677b6904db58827
Dries Kimpe <dkimpe@mcs.anl.gov>
2010-01-18 13:22:59 -0600

    Add operator < for handles to zoidfs-ops.hh + fix
    * Inline keyword was missing
    * Added operator <

--- src/zoidfs/util/zoidfs-ops.hh
+++ src/zoidfs/util/zoidfs-ops.hh
@@ -11,14 +11,21 @@ namespace zoidfs
 #define TMP_EQUAL_CMP(name) (t1.name == t2.name)
 
 /**
+ * Operator < for zoidfs_handle_t 
+ */
+inline bool operator < (const zoidfs_handle_t & h1, const zoidfs_handle_t & h2)
+{
+   return memcmp (&h1.data, &h2.data, sizeof(h1.data));
+}
+/**
  * operator == for zoidfs types.
  */
-bool operator == (const zoidfs_time_t & t1, const zoidfs_time_t & t2)
+inline bool operator == (const zoidfs_time_t & t1, const zoidfs_time_t & t2)
 {
    return (t1.seconds == t2.seconds) && (t1.nseconds == t2.nseconds);
 }
 
-bool operator == (const zoidfs_attr_t & t1, const zoidfs_attr_t & t2)
+inline bool operator == (const zoidfs_attr_t & t1, const zoidfs_attr_t & t2)
 {
    // doesn't contain a string or so, so just use memcmp
    return TMP_EQUAL_CMP(type)
@@ -36,13 +43,13 @@ bool operator == (const zoidfs_attr_t & t1, const zoidfs_attr_t & t2)
       && TMP_EQUAL_CMP(ctime);
 }
 
-bool operator == (const zoidfs_handle_t & t1, const zoidfs_handle_t & t2)
+inline bool operator == (const zoidfs_handle_t & t1, const zoidfs_handle_t & t2)
 {
    return !memcmp (&t1.data[0], &t2.data[0], sizeof (t1.data));
 }
 
 
-bool operator == (const zoidfs_cache_hint_t & t1, const zoidfs_cache_hint_t & t2)
+inline bool operator == (const zoidfs_cache_hint_t & t1, const zoidfs_cache_hint_t & t2)
 {
    return TMP_EQUAL_CMP(size)
       && TMP_EQUAL_CMP(atime)
@@ -50,7 +57,7 @@ bool operator == (const zoidfs_cache_hint_t & t1, const zoidfs_cache_hint_t & t2
       && TMP_EQUAL_CMP(mtime);
 }
 
-bool operator == (const zoidfs_sattr_t & t1, const zoidfs_sattr_t & t2)
+inline bool operator == (const zoidfs_sattr_t & t1, const zoidfs_sattr_t & t2)
 {
    return TMP_EQUAL_CMP(mask)
       && TMP_EQUAL_CMP(mode)
@@ -61,7 +68,7 @@ bool operator == (const zoidfs_sattr_t & t1, const zoidfs_sattr_t & t2)
       && TMP_EQUAL_CMP(mtime);
 }
 
-bool operator == (const zoidfs_dirent_t & t1, const zoidfs_dirent_t & t2)
+inline bool operator == (const zoidfs_dirent_t & t1, const zoidfs_dirent_t & t2)
 {
    return !strncmp (t1.name, t2.name, ZOIDFS_NAME_MAX)
       && t1.handle == t2.handle
