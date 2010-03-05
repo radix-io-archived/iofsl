496fd9885949436c6fb343a3fec7765870b34a82
Jason Cope <copej@mcs.anl.gov>
2010-03-05 10:12:30 -0600

    added InjectPool template to NotImplementedTask

--- src/iofwd/NotImplementedTask.hh
+++ src/iofwd/NotImplementedTask.hh
@@ -5,6 +5,7 @@
 #include "Task.hh"
 #include "NotImplementedRequest.hh"
 #include "TaskHelper.hh"
+#include "iofwdutil/InjectPool.hh"
 
 namespace iofwd
 {
@@ -12,7 +13,7 @@ namespace iofwd
 
 
 class NotImplementedTask :
-   public TaskHelper<NotImplementedRequest>
+   public TaskHelper<NotImplementedRequest>, public iofwdutil::InjectPool<NotImplementedTask>
 {
 public:
    NotImplementedTask (ThreadTaskParam & p)
