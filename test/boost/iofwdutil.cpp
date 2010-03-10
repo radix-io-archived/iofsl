d49b94d56c612999d81cfabfbc372b80e67242cd
Dries Kimpe <dkimpe@mcs.anl.gov>
2010-03-10 17:42:50 -0600

    Added unit test base for iofwdutil

---
+++ test/boost/iofwdutil.cpp
@@ -0,0 +1,16 @@
+
+#define BOOST_TEST_MODULE "iofwdutil"
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+struct Dummy {};
+
+BOOST_AUTO_TEST_SUITE( iofwdutil_dummy )
+
+BOOST_FIXTURE_TEST_CASE( iofwdutil_dummy, Dummy )
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
