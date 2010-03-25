854cb2241e3ef359ba3b0575dd2cbe73981452f3
Dries Kimpe <dkimpe@mcs.anl.gov>
2010-03-25 15:40:20 -0500

    Fix bug in Singleton
    Singleton wasn't safe it is was called from within another static
    initialization function. The problem was that Singleton contained a
    non-pod static scoped_ptr variable, and there was no guarantee about when its
    initialization function (setting the scoped_ptr to 0) was ran.
    
    If it ran before the static initialization function that used the
    singleton, singleton was used the ptr uninitialized; In addition, at
    some point after that the constructor of scoped_ptr would run, resetting
    ptr to 0 while the init_flag was set to true; the next user of the
    singleton would get a 0 ref...

--- src/iofwdutil/Singleton.hh
+++ src/iofwdutil/Singleton.hh
@@ -3,86 +3,86 @@
 
 #include <boost/utility.hpp>
 #include <boost/scoped_ptr.hpp>
-
-
-#ifdef USE_OWN_SINGLETON
-#include <csignal>
-#include <boost/thread.hpp>
-#else
 #include <boost/thread/once.hpp>
-#endif
+
+#include <cassert>
 
 namespace iofwdutil
 {
 //===========================================================================
 
+/**
+ * Usage:
+ *   Derive the class you want to make a singleton from Singleton<CLASS>
+ *   i.e.
+ *
+ *   class MySingleton : public Singleton<MySingleton>
+ *   {
+ *     ...
+ *
+ *   This injects a static 'instance()' method in the class which can
+ *   be used to obtain a reference to the singleton instance.
+ *   
+ *   Note: Singleton is threadsafe.
+ *
+ *   @TODO Add valgrind ignore macro for memory allocated here.
+ */
 template<class T>
 class Singleton : private boost::noncopyable
 {
-#ifdef USE_OWN_SINGLETON
-protected:
-   enum { NOT_INITIALIZED = 0,
-          INITIALIZED     = 2 };
-#endif
-
 public:
 
-#ifdef USE_OWN_SINGLETON
-   static void build ()
-   {
-      boost::mutex::scoped_lock l(lock_);
-      // Check again in case more than one thread made it into build.
-      if (status_ != INITIALIZED)
-         return;
-
-      init ();
-      status_ = INITIALIZED;
-   }
-
-    static T& instance()
-    {
-       if (status_ != INITIALIZED)
-          build ();
-
-       return *t;
-    }
-#else
  static T& instance()
     {
         boost::call_once(init, flag);
+        assert (t);
         return *t;
     }
-#endif
 
     static void init() // never throws
     {
-        t.reset(new T());
+       assert (t == 0);
+       t = new T();
     }
 
 protected:
-    ~Singleton() {}
+    ~Singleton()
+    {
+    }
+
      Singleton() {}
 
 private:
-     static boost::scoped_ptr<T> t;
-
-#ifdef USE_OWN_SINGLETON
-     static sig_atomic_t status_;
-     static boost::mutex lock_;
-#else
-  static boost::once_flag flag;
-#endif
+     static T * t;
+     static boost::once_flag flag;
 };
 
-template<class T> boost::scoped_ptr<T> iofwdutil::Singleton<T>::t(0);
+/*
+ * We cannot use a scoped_ptr or any other non-pod type for T or
+ * boost::once_flag because otherwise the initialization function for that
+ * type might be called after the singleton was initialized.
+ *
+ * i.e. if the singleton is used from within a function keyed to global
+ * constructors of static variables, the singleton might access 't' or 'flag'
+ * without it being initialized. 
+ *
+ * In addition, if later the initialization function for 't' or 'flag' runs,
+ * it would overwrite their value.
+ */
 
-#ifdef USE_OWN_SINGLETON
-template<class T> boost::mutex iofwdutil::Singleton<T>::lock_;
-template<class T> sig_atomic_t iofwdutil::Singleton<T>::status_ =
-                           iofwdutil::Singleton<T>::NOT_INITIALIZED;
-#else
+/*
+Objects with static storage duration (3.7.1) shall be zero-initialized (8.5)
+before any other initialization takes place. Zero-initialization and
+initialization with a constant expression are collectively called static
+initialization; all other initialization is dynamic initialization. Objects of
+POD types (3.9) with static storage duration initialized with constant
+expressions (5.19) shall be initialized before any dynamic initialization
+takes place. Objects with static storage duration defined in namespace scope
+in the same translation unit and dynamically initialized shall be initialized
+in the order in which their definition appears in the translation unit.
+*/
+template<class T> T * iofwdutil::Singleton<T>::t;
 template<class T> boost::once_flag iofwdutil::Singleton<T>::flag = BOOST_ONCE_INIT;
-#endif
 
 
 //===========================================================================
