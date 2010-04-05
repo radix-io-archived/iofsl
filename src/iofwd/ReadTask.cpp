ee8653d7ce5a62d86ade517ac624f6efdbf21853
Jason Cope <copej@mcs.anl.gov>
2010-04-05 17:41:31 -0500

    added pipeline support to the ReadTask.
    Pipeline support is now more like the state machines...
    we precompute the expected buffers and issue the zoidfs IO
    requests, and send data to the clients as the zoidfs io finishes

--- src/iofwd/ReadTask.cpp
+++ src/iofwd/ReadTask.cpp
@@ -34,15 +34,246 @@ void ReadTask::runNormalMode(const ReadRequest::ReqParam & p)
    block_.wait();
 }
 
-void ReadTask::execPipelineIO(const ReadRequest::ReqParam & UNUSED(p))
+void ReadTask::computePipelineFileSegments(const ReadRequest::ReqParam & p)
 {
+    const zoidfs::zoidfs_file_ofs_t * file_starts = p.file_starts;
+    const zoidfs::zoidfs_file_size_t * file_sizes = p.file_sizes;
+
+    size_t cur_pipe_ofs = 0;
+    int cur_file_index = 0;
+    int num_pipe_segments = 0;
+    size_t pipeline_size = bpool_->pipeline_size();
+    size_t cur_pipe_buffer_size = pipeline_size;
+    zoidfs::zoidfs_file_size_t cur_file_size = file_sizes[cur_file_index];
+    zoidfs::zoidfs_file_ofs_t cur_file_start = file_starts[cur_file_index];
+    size_t cur_mem_offset = 0;
+    int cur_pipe = 0;
+
+    /* while there is still data to process in this op */
+    p_segments.push_back(0);
+    p_segments_start.push_back(0);
+    while(cur_pipe_ofs < total_bytes_)
+    {
+        /* setup the file sizes */
+        cur_file_size = file_sizes[cur_file_index];
+        cur_file_start = file_starts[cur_file_index];
+
+        while(cur_file_size > 0)
+        {
+            /* if the data left in the current file is large than the remaining pipeline buffer */
+            if(cur_file_size > cur_pipe_buffer_size)
+            {
+                /* update the pipeline segment data */
+                p_file_sizes.push_back(cur_pipe_buffer_size);
+                p_file_starts.push_back(cur_file_start);
+                p_mem_offsets.push_back(cur_mem_offset);
+
+                p_segments[cur_pipe] += 1;
+
+                cur_file_start += cur_pipe_buffer_size;
+
+                cur_file_size -= cur_pipe_buffer_size;
+                cur_pipe_ofs += cur_pipe_buffer_size;
+                cur_pipe_buffer_size = pipeline_size;
+                cur_pipe++;
+                p_segments.push_back(0);
+                cur_mem_offset = 0;
+                num_pipe_segments++;
+                p_segments_start.push_back(num_pipe_segments);
+            }
+            else
+            {
+                /* update the pipeline segment data */
+                p_file_sizes.push_back(cur_file_size);
+                p_file_starts.push_back(cur_file_start);
+                p_mem_offsets.push_back(cur_mem_offset);
+
+                p_segments[cur_pipe] += 1;
+
+                cur_file_start += cur_file_size;
+
+                cur_pipe_buffer_size -= cur_file_size;
+                cur_pipe_ofs += cur_file_size;
+                cur_mem_offset += cur_file_size;
+                cur_file_size = 0;
+                cur_file_index++;
+                num_pipe_segments++;
+            }
+        }
+    }
 }
 
-void ReadTask::runPipelineMode(const ReadRequest::ReqParam & UNUSED(p))
+void ReadTask::getBMIBuffer(int index)
 {
-   // send reply w/ callback
+    /* request a BMI buffer */
+    block_.reset();
+    bpool_->allocCB(block_, request_.getRequestAddr(), iofwdutil::bmi::BMI::ALLOC_SEND, rbuffer_[index]->buffer);
 
-   request_.setReturnCode(zoidfs::ZFSERR_NOTIMPL);
+    /* set the callback and wait */
+    block_.wait();
+}
+
+void ReadTask::sendPipelineBuffer(int index)
+{
+    /* if there is still data to be recieved */
+    block_.reset();
+    p_siz_ = std::min((size_t)bpool_->pipeline_size(), total_bytes_ - cur_sent_bytes_);
+    request_.sendPipelineBufferCB(block_, rbuffer_[index]->buffer->get_buf(), p_siz_);
+
+    /* set the callback and wait */
+    block_.wait();
+
+    /* update the byte transfer count */
+    cur_sent_bytes_ += p_siz_;
+}
+
+void ReadTask::execPipelineIO(const ReadRequest::ReqParam & p)
+{
+    int index = 0;
+    int pipe_read_ops_posted = total_pipeline_ops_;
+    int pipe_read_ops_done = total_pipeline_ops_;
+    int pipe_sent_ops = total_pipeline_ops_;
+    int pipe_buffer_ops = total_pipeline_ops_;
+
+    /* while the pipeline operation is not done... */
+    while( pipe_read_ops_posted > 0 || pipe_read_ops_done > 0 || pipe_buffer_ops > 0 )
+    {
+        /* get a buffer */
+        if(pipe_buffer_ops > 0)
+        {
+            getBMIBuffer(index);
+            pipe_buffer_ops--;
+        }
+
+        /* post a read op */
+        if(pipe_read_ops_posted > 0)
+        {
+            postRead(p, index);
+            pipe_read_ops_posted--;
+
+            /* add the pipeline op to the lookup vector */
+            pipeline_ops_.push_back(index);
+
+            /* advance to the next pipeline op */
+            index++;
+        }
+
+        /* check and see if any pending reads completed */
+        if(pipe_read_ops_done > 0)
+        {
+            /* iterate over the pipeline op vector */
+            unsigned int i = 0;
+            while(i < pipeline_ops_.size())
+            {
+                /* get the index we are going to test */
+                int cur = pipeline_ops_[i];
+
+                /* if this block completed */
+                if(pipeline_blocks_[cur]->test())
+                {
+                    /* reset the pipeline block */
+                    pipeline_blocks_[cur]->wait();
+
+                    /* update the count */
+                    pipe_read_ops_done--;
+
+                    /* remove the completed op from the pipeline op list */
+                    pipeline_ops_.erase(pipeline_ops_.begin() + i);
+                }
+                else
+                {
+                    /* update the counter */
+                    i++;
+                }
+            }
+        }
+    }
+
+    /* check for completed read ops after all of the ops have been posted */
+    while(pipe_read_ops_done > 0)
+    {
+        /* iterate over the pipeline op vector */
+        unsigned int i = 0;
+        while(i < pipeline_ops_.size())
+        {
+            /* get the index we are going to test */
+            int cur = pipeline_ops_[i];
+
+            /* if this block completed */
+            if(pipeline_blocks_[cur]->test())
+            {
+                /* reset the pipeline block */
+                pipeline_blocks_[cur]->reset();
+
+                /* update the count */
+                pipe_read_ops_done--;
+
+                /* remove the completed op from the pipeline op list */
+                pipeline_ops_.erase(pipeline_ops_.begin() + i);
+            }
+            else
+            {
+                /* update the counter */
+                i++;
+            }
+        }
+    }
+
+    /* send the data to the client */
+    int i = 0;
+    while(pipe_sent_ops > 0)
+    { 
+        /* sent data into a buffer */
+        sendPipelineBuffer(i);
+        pipe_sent_ops--;
+        i++;
+    }
+}
+
+void ReadTask::postRead(const ReadRequest::ReqParam & p, int index)
+{
+    const char * p_buf = (char *)rbuffer_[index]->buffer->get_buf()->get();
+    int p_seg_start = p_segments_start[index];
+    int p_file_count = p_segments[index];
+    int * ret = new int(0);
+
+    /* setup segment data structures */
+    zoidfs::zoidfs_file_ofs_t * file_starts = new zoidfs::zoidfs_file_ofs_t[p_file_count];
+    zoidfs::zoidfs_file_size_t * file_sizes = new zoidfs::zoidfs_file_size_t[p_file_count];
+    const char ** mem_starts = new const char*[p_file_count];
+    size_t * mem_sizes = new size_t[p_file_count];
+
+    for (int i = 0; i < p_file_count; i++)
+    {
+        /* find the index into the pipeline data */
+        int p_index = p_seg_start + i;
+
+        /* compute the I/O offsets for this data */
+        mem_starts[i] = p_buf + p_mem_offsets[p_index];
+        mem_sizes[i] = p_file_sizes[p_index];
+        file_starts[i] = p_file_starts[p_index];
+        file_sizes[i] = p_file_sizes[p_index];
+    }
+
+    rbuffer_[index]->mem_starts = mem_starts;
+    rbuffer_[index]->mem_sizes = mem_sizes;
+    rbuffer_[index]->file_starts = file_starts;
+    rbuffer_[index]->file_sizes = file_sizes;
+    rbuffer_[index]->ret = ret;
+
+    /* enqueue the read */
+    sched_->enqueueReadCB (
+        *(pipeline_blocks_[index]), p.handle, p_file_count, (void**)mem_starts, mem_sizes,
+        file_starts, file_sizes, p.op_hint);
+}
+
+void ReadTask::runPipelineMode(const ReadRequest::ReqParam & p)
+{
+   // run the pipeline IO transfer code
+   execPipelineIO(p);
+
+   // reply status
+   request_.setReturnCode(zoidfs::ZFS_OK);
    block_.reset();
    request_.reply((block_));
    block_.wait();
--- src/iofwd/ReadTask.hh
+++ src/iofwd/ReadTask.hh
@@ -1,11 +1,16 @@
 #ifndef IOFWD_READTASK_HH
 #define IOFWD_READTASK_HH
 
-#include "Task.hh"
-#include "ReadRequest.hh"
-#include "TaskHelper.hh"
+#include <vector>
+#include "iofwd/Task.hh"
+#include "iofwd/ReadRequest.hh"
+#include "iofwd/TaskHelper.hh"
+#include <boost/function.hpp>
 #include "iofwdutil/InjectPool.hh"
 #include "iofwdutil/tools.hh"
+#include "iofwdevent/SingleCompletion.hh"
+#include "iofwd/RetrievedBuffer.hh"
+#include "zoidfs/zoidfs.h"
 
 namespace iofwd
 {
@@ -14,11 +19,32 @@ class ReadTask : public TaskHelper<ReadRequest>, public iofwdutil::InjectPool<Re
 {
 public:
    ReadTask (ThreadTaskParam & p)
-      : TaskHelper<ReadRequest>(p)
+      : TaskHelper<ReadRequest>(p), total_bytes_(0), cur_sent_bytes_(0), p_siz_(0), total_pipeline_ops_(0),
+        rbuffer_(NULL), pipeline_blocks_(NULL)
    {
    }
+
    virtual ~ReadTask()
    {
+        /* cleanup rbuffer_ wrappers */
+        for(int i = 0 ; i < total_pipeline_ops_ ; i++)
+        {
+            delete rbuffer_[i];
+        }
+        delete [] rbuffer_;
+
+        /* cleanup up the pipeline blocks */
+        for(int i = 0 ; i < total_pipeline_ops_ ; i++)
+        {
+            pipeline_blocks_[i]->reset();
+            delete pipeline_blocks_[i];
+        }
+
+        /* only delete the pipeline blocks if this was a pipeline op */
+        if(total_pipeline_ops_ > 0)
+        {
+            delete [] pipeline_blocks_;
+        }
    }
 
    void run ()
@@ -28,13 +54,61 @@ public:
       if (p.pipeline_size == 0)
          runNormalMode(p);
       else
-         runPipelineMode(p);
+      {
+            /* compute the total size of the pipeline transfers */
+            for (size_t i = 0; i < p.mem_count; i++)
+                total_bytes_ += p.mem_sizes[i];
+
+            /* compute the total number of concurrent pipeline ops */
+            total_pipeline_ops_ = (int)ceil(1.0 * total_bytes_ / bpool_->pipeline_size());
+
+            computePipelineFileSegments(p);
+
+            /* setup the rbuffer variable */
+            rbuffer_ = new RetrievedBuffer*[total_pipeline_ops_];
+            for(int i = 0 ; i < total_pipeline_ops_ ; i++)
+            {
+                rbuffer_[i] = new RetrievedBuffer();
+                rbuffer_[i]->reinit();
+            }
+
+            /* setup the pipeline blocks */
+            pipeline_blocks_ = new iofwdevent::SingleCompletion*[total_pipeline_ops_];
+            for(int i = 0 ; i < total_pipeline_ops_ ; i++)
+            {
+                pipeline_blocks_[i] = new iofwdevent::SingleCompletion();
+            }
+
+            /* run the pipeline code */
+            runPipelineMode(p);
+      }
    }
 
 private:
    void runNormalMode(const ReadRequest::ReqParam & p);
    void runPipelineMode(const ReadRequest::ReqParam & p);
    void execPipelineIO(const ReadRequest::ReqParam & p);
+   void postRead(const ReadRequest::ReqParam & p, int index);
+   void getBMIBuffer(int index);
+   void computePipelineFileSegments(const ReadRequest::ReqParam & p);
+   void sendPipelineBuffer(int index);
+
+   /* pipeline variables */
+   size_t total_bytes_;
+   size_t cur_sent_bytes_;
+   size_t p_siz_;
+   int total_pipeline_ops_;
+   RetrievedBuffer ** rbuffer_;
+   iofwdevent::SingleCompletion ** pipeline_blocks_;
+
+   /* @TODO: we only need these vectors when we pipeline... maybe we need seperate pipeline and normal tasks
+      so that we can save so mem and alloc time when we are in normal mode ? */
+   std::vector<zoidfs::zoidfs_file_size_t> p_file_sizes;
+   std::vector<zoidfs::zoidfs_file_ofs_t> p_file_starts;
+   std::vector<size_t> p_mem_offsets;
+   std::vector<int> p_segments;
+   std::vector<int> p_segments_start;
+   std::vector<int> pipeline_ops_;
 };
 
 }
