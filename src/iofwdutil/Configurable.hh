98d8ca5c76e739ed783d0cabe238575d34fbd80b
Dries Kimpe <dkimpe@mcs.anl.gov>
2010-04-28 16:21:48 -0500

    Fixed nasty bug in Configurable
    Configurable::configure_if_needed was catching bad_cast to see
    if the underlying class supported the Configurable interface.
    Unfortunately, it was also calling the configure method of the class
    within its try block. As such, it would catch and hide any bad_cast
    exception (or derived!) and act as if the underlying object did
    not support configuration.

--- src/iofwdutil/Configurable.hh
+++ src/iofwdutil/Configurable.hh
@@ -29,14 +29,24 @@ namespace iofwdutil
    void Configurable::configure_if_needed (T * cfg, const ConfigFile & config)
    {
       // See if the object implements the interface;
+      Configurable * ptr = 0;
       try
       {
-         dynamic_cast<Configurable *>(cfg)->configure (config);
+         ptr = dynamic_cast<Configurable *>(cfg);
       }
       catch (std::bad_cast & bd)
       {
          // It doens't: no need to configure
+         return;
       }
+      // NOTE: it is important that this step is *outside* of the try block;
+      // Otherwise, if anything in the configure method throws a bad_cast, we
+      // will hide the exception and continue as if nothing happened.
+      //
+      // Note that bad_lexical_cast derives from bad_cast, so any errors
+      // caused by getKeyAs in configure would just be dropped if we'd try to
+      // configure within the try block above!
+      ptr->configure (config);
    }
 
 }
