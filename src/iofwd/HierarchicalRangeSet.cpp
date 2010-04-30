ca567827fccb7eeeaa673d8d3c2222061f03bbb0
Jason Cope <copej@mcs.anl.gov>
2010-04-30 17:39:09 -0500

    removed some duplicate code from the h range set

--- src/iofwd/HierarchicalRangeSet.cpp
+++ src/iofwd/HierarchicalRangeSet.cpp
@@ -1,12 +1,5 @@
-#include "iofwd/HierarchialRangeSet.hh"
+#include "iofwd/HierarchicalRangeSet.hh"
 #include "iofwdutil/LinkHelper.hh"
-
-/* make this range set configurable */
-GENERIC_FACTORY_CLIENT(std::string,
-      iofwd::BaseRangeSet,
-      iofwd::HierarchicalRangeSet,
-      "hierarchical",
-      hierarchical);
 
 /* make this range set configurable */
 GENERIC_FACTORY_CLIENT(std::string,
