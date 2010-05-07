a6b8355f1bf87aea2281871fe67c5ecdfc019419
Dries Kimpe <dkimpe@mcs.anl.gov>
2010-05-06 21:41:46 -0500

    Complete backtrace code.
    Could be used in the base constructor of fatal exceptions.

--- src/iofwdutil/backtrace.cpp
+++ src/iofwdutil/backtrace.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include "iofwd_config.h"
 
 #ifdef HAVE_EXECINFO_H
@@ -6,8 +7,31 @@
 
 #include "backtrace.hh"
 
+using namespace std;
+
 namespace iofwdutil
 {
+   //========================================================================
 
 
+   void getStackTrace (std::vector<std::string> & vec, size_t maxdepth)
+   {
+#ifdef HAVE_EXECINFO_H
+      std::vector<void *> addrs (maxdepth);
+      size_t count = backtrace (&addrs[0], addrs.size());
+      char ** symbols = backtrace_symbols(&addrs[0], count);
+      vec.reserve (count);
+      vec.clear();
+      for (size_t i=0; i<count; ++i)
+      {
+         vec.push_back (symbols[i]);
+      }
+
+      free(symbols);
+#else
+      vec.clear ();
+#endif
+   }
+
+   //========================================================================
 }
--- src/iofwdutil/backtrace.hh
+++ src/iofwdutil/backtrace.hh
@@ -1,8 +1,19 @@
 #ifndef IOFWDUTIL_BACKTRACE_HH
 #define IOFWDUTIL_BACKTRACE_HH
 
+#include <vector>
+#include <string>
+
 namespace iofwdutil
 {
+
+   /**
+    * Fill the vector with the stack trace.
+    * Returns an empty vector if no stack traces can be generated on this
+    * system.
+    */
+   void getStackTrace (std::vector<std::string> & vec, 
+         size_t maxdepth = 24);
 
 
 }
