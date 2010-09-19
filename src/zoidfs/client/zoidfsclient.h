f3d2746b23c6de2e5e06fedc9d04df5b5a9f0922
Jason Cope <copej@mcs.anl.gov>
2010-09-19 16:43:37 -0500

    added func prototypes used by other zoidfs client libs

---
+++ src/zoidfs/client/zoidfsclient.h
@@ -0,0 +1,10 @@
+#ifndef ZOIDFSCLIENT_H
+#define ZOIDFSCLIENT_H
+
+void zoidfs_client_swap_addr(BMI_addr_t * naddr);
+void zoidfs_client_def_addr(void);
+void zoidfs_client_set_def_addr(BMI_addr_t * addr);
+bmi_context_id * zoidfs_client_get_context();
+void zoidfs_client_set_pipeline_size(size_t psize);
+
+#endif
