13249745ad6d9865748d6531813008a8869fd1fb
Jason Cope <copej@mcs.anl.gov>
2010-12-02 16:14:16 -0600

    bug fix: needed zoidfs_client_set_pipeline_size in the include file

--- src/zoidfs/client/zoidfsclient.h
+++ src/zoidfs/client/zoidfsclient.h
@@ -6,5 +6,6 @@ void zoidfs_client_def_addr(void);
 void zoidfs_client_set_def_addr(BMI_addr_t * addr);
 bmi_context_id * zoidfs_client_get_context();
 void zoidfs_client_set_pipeline_size(size_t psize);
+unsigned int zoidfs_xdr_size_processor_cache_init();
 
 #endif
