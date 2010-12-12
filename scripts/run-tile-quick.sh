75348965ba856259afb9508eef3ee0435bac4e96
Jason Cope <copej@mcs.anl.gov>
2010-12-12 15:31:27 -0600

    fix to tile-io run script

--- scripts/run-tile-quick.sh
+++ scripts/run-tile-quick.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/hgfs/mac-copej/work/anl/mcs/iofwd-64bit/boost-1.39.0/lib:/mnt/hgfs/mac-copej/work/anl/mcs/iofwd-64bit/cunit-2.1-0/lib
+IOFSL_DIR="."
+OUTFILE="/tmp/testfile"
 export ZOIDFS_ION_NAME=tcp://127.0.0.1:1234
 
 itr=1
@@ -10,7 +11,7 @@ do
     sumsq=0.0
     for i in $(seq 1 $itr)
     do
-        val=`~/mac-copej/work/anl/mcs/iofwd-64bit/mpich2-1.3a2/bin/mpiexec -n 4 ./test/zoidfs-microbenchmarks/zoidfs-tile-io --nr_tiles_x 1 --nr_tiles_y 4 --sz_tile_x $j --sz_tile_y $j --sz_element 262144 --write_file --cleanup --filename /tmp/testfile | grep "Write Band" | awk '{print $4}'`
+        val=`mpiexec -n 4 $IOFSL_DIR/test/zoidfs-microbenchmarks/zoidfs-tile-io --nr_tiles_x 1 --nr_tiles_y 4 --sz_tile_x $j --sz_tile_y $j --sz_element 262144 --write_file --cleanup --filename $OUTFILE | grep "Write Band" | awk '{print $4}'`
         sum=`echo "$sum + $val" | bc -l`
     done
     avg=`echo "$sum / $itr" | bc -l`
