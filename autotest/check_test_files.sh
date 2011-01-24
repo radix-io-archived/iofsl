4349498b59ab3be2a5e82d8af36c58232ef52026
Rico D'Amore <rjdamore@lanl.gov>
2011-01-24 08:51:02 -0700

    streamlined commit-testing and added error email formatting
    git-repo-tests.sh is a refined commit-test.sh, still more changes to
    come.  Added better email format for error reporting to committer and
    IOFWD-lists.

---
+++ autotest/check_test_files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+read_failures(){
+if [[ -s ~/iofsl/failure_report.txt ]]; then
+grep "committed" ../failure_report.txt | awk '{print $5}' > failure_emails.txt
+else
+echo "failure_report.txt is empty."
+fi;
+failures_to_array
+}
+
+failures_to_array(){
+failures_IFS=$IFS
+IFS=$'\n'
+failures=($(cat failure_emails.txt))
+IFS=$failures_IFS
+mail_failures
+}
+
+mail_failures(){
+for i in "${failures[@]}"
+do
+mail -s "IOFSL commit test failure report" $i < ~/iofsl/failure_report.txt 
+done  
+}
+
+if [[ -s ~/iofsl/test_report.txt ]]; then
+mail -s "IOFSL commits test report" rjdamore@lanl.gov < ~/iofsl/test_report.txt
+else
+echo "test_report.txt is empty"
+fi;
+read_failures
