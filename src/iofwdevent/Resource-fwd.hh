1787a02fc812cea8fd90affc9ccd04453234af20
Dries Kimpe <dkimpe@mcs.anl.gov>
2011-01-29 20:49:30 -0600

    Header for forward declaration of common resources
    Improve compile time by using forward header whenever only
    reference/pointer is needed in a header.

---
+++ src/iofwdevent/Resource-fwd.hh
@@ -0,0 +1,14 @@
+#ifndef IOFWDEVENT_RESOURCE_FWD_HH
+#define IOFWDEVENT_RESOURCE_FWD_HH
+
+namespace iofwdevent
+{
+   //=========================================================================
+
+   class BMIResource;
+   class TimerResource;
+
+   //=========================================================================
+}
+
+#endif
