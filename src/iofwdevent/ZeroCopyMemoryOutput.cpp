1d70b5edea8b763da2643a57bcb9d185ce7783dc
Benjamin Welton <welton@mcs.anl.gov>
2011-02-10 10:07:44 -0600

    Added output zero copy memmory stream

---
+++ src/iofwdevent/ZeroCopyMemoryOutput.cpp
@@ -0,0 +1,90 @@
+#include "ZeroCopyMemoryOutput.hh"
+using namespace boost;
+namespace iofwdevent {
+
+/**
+ * Constructor for ZeroCopyMemoryOutput.
+ * @param[in] in Memory region to use for the Input Stream
+ * @param[in] len Length of the memory region void * in.
+ */
+ZeroCopyMemoryOutput::ZeroCopyMemoryOutput (void ** output, size_t len)
+{
+  char * in = new char[len];
+  this->output = output;
+  this->mem.reset(in);
+  this->pos = 0;
+  this->memSize = len;
+  this->offset = 0;
+}
+
+
+/**
+ * Write to the stream returning a pointer to the region of memory where 
+ * the data can be written to. 
+ * @param[out] ptr void ** pointer containing the address to the data that was 
+ *                 written
+ * @param[out] size Number of bytes that can be written to the stream.
+ * @param[in]  cb Callback function to be used when operation is completed
+ * @param[in]  suggested Client specified suggested read size. This write size 
+ *                      is best effort and the actual write may be more or less
+ *                      then the value specified in suggested.
+ * @return Returns a handle relating to this particular write. (not used with
+ *         ZeroCopyMemoryOutput due to no blocking in this class)
+ */
+Handle ZeroCopyMemoryOutput::write (void ** ptr, size_t * size, 
+                                    const CBType & cb, size_t suggested)
+{
+  if (suggested > this->memSize - this->pos)
+  {
+    (*ptr) = this->mem.get() + (this->pos);
+    (*size) = this->memSize - this->pos;
+    this->pos = this->memSize;
+  }
+  else 
+  {
+    (*ptr) = this->mem.get() + (this->pos);
+    (*size) = suggested;
+    this->pos = this->pos + suggested;
+  }
+  /* Call the callback */
+  cb(*(new CBException()));
+  /* Do not return a handle with this call (non-blocking) */
+  return (void *)0;  
+}
+
+/**
+ * Allows for the stream to be "rewinded" and unused portions of a write to be
+ * returned to ZeroCopyMemoryOutput. 
+
+ * @param[in] size How much to rewind.
+ * @param[in] cb Callback to use when the operation is completed (called 
+ *               immedietly).
+ * @return Returns a handle relating to this particular rewind. (not used with
+ *         ZeroCopyMemoryOutput due to no blocking in this class)
+ */
+Handle ZeroCopyMemoryOutput::rewindOutput (size_t size, const CBType & cb)
+{
+  if (size - this->pos >= this->offset)
+    this->pos = this->pos - size;
+  cb(*(new CBException()));
+  return (void *)0; 
+}
+
+/**
+ * Flush the internal buffer (permanently advance the pointer).
+ * @param[in] cb Callback to use when the operation is completed (called 
+ *               immedietly).
+ * @return Returns a handle relating to this particular flush. (not used with
+ *         ZeroCopyMemoryOutput due to no blocking in this class)
+ */
+Handle ZeroCopyMemoryOutput::flush (const CBType & cb)
+{
+  /* Make the lower bound = to the current pointer position. Consider memory 
+     before offset to be "written" and unchangable */
+  *((char **)this->output) = this->mem.get() + this->offset;
+  this->offset = this->pos;
+  cb(*(new CBException()));
+  return (void *)0; 
+}
+
+}
