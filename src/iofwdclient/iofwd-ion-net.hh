8d12676afb7cde06fb1d7054c552eff4de5625e6
Jason Cope <copej@mcs.anl.gov>
2011-02-11 17:52:39 -0600

    added skeleton code for C++ rpc client

---
+++ src/iofwdclient/iofwd-ion-net.hh
@@ -0,0 +1,13 @@
+#include "src/net/Address.hh"
+#include "src/net/Lookup.hh"
+/* function that returns addr from string */
+net::address getAddr()
+{
+}
+
+/* function that returns in and out streams to the ion given addr */
+void connect(net::address addr)
+{
+}
+
+/* hack BMI to have "release addr" capability */
