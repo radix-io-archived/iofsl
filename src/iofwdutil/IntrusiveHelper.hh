1499e94daff5a480028ed951af7270291052e9a0
Dries Kimpe <dkimpe@mcs.anl.gov>
2011-02-17 11:32:59 -0800

    Fix IntrusiveHelper for intrusive_ptr<const T>
    - Added checked_delete
    - intrusive_ptr<const T> wasn't working correctly

--- src/iofwdutil/IntrusiveHelper.hh
+++ src/iofwdutil/IntrusiveHelper.hh
@@ -4,70 +4,79 @@
 #include "iofwdutil/assert.hh"
 #include "iofwdutil/atomics.hh"
 
+#include <boost/utility.hpp>
+
 namespace iofwdutil
 {
-
-/**
- * Helper class to use with boost intrusive_ptr
- *
- * Usage: derive your class from this class,
- * and add the macro INTRUSIVE_PTR_HELPER(yourclassname);
- * in the class header.
- *
- * Now you can use your class with the boost intrusive shared pointer.
- */
-class IntrusiveHelper 
-{
-public:
-   IntrusiveHelper () :
-      refcount_ (0)
-   {
-   }
-
-   ~IntrusiveHelper ()
-   {
-      /* Make sure nobody is still pointing to us */
-      ASSERT(!refcount_);
-   }
-
-   int removeref ()
-   {
-      ASSERT(refcount_);
-      return --refcount_;
-   }
-
-   int addref ()
-   {
-      return ++refcount_;
-   }
+   //========================================================================
 
    /**
-    * Returns true if there are still shared pointers pointing to us.
+    * Helper class to use with boost intrusive_ptr
+    *
+    * Usage: derive your class from this class,
+    * and add the macro INTRUSIVE_PTR_HELPER(yourclassname);
+    * in the class header.
+    *
+    * Now you can use your class with the boost intrusive shared pointer.
+    *
+    * !!! DO NOT DERIVE FROM CLASSES THAT DERIVE FROM INTRUSIVEHELPER !!!
+    * (unless the base class has a virtual destructor)
+    *
+    * Note: the reference count update functions are const since we need to be
+    * able to support intrusive_ptr<const T>
     */
-   int alive () const
+   class IntrusiveHelper
    {
-      return refcount_;
-   }
-private:
-   atomic<int> refcount_;
-};
+      public:
+         IntrusiveHelper ()
+            : refcount_ (0)
+         {
+         }
+
+         ~IntrusiveHelper ()
+         {
+            /* Make sure nobody is still pointing to us */
+            ASSERT(!refcount_);
+         }
+
+         int removeref () const
+         {
+            ASSERT(refcount_);
+            return --refcount_;
+         }
+
+         int addref () const
+         {
+            return ++refcount_;
+         }
+
+         /**
+          * Returns true if there are still shared pointers pointing to us.
+          */
+         int alive () const
+         {
+            return refcount_;
+         }
+
+      private:
+         mutable atomic<int> refcount_;
+   };
 
 
-/* @NOTE:
- *    * Can simplify this by providing a template version
- *      of the functions below.
- *    * The code below is threadsafe since the reference count is
- *      stored in an atomic<int> var.
- */
+   /* @NOTE:
+    *    * Can simplify this by providing a template version
+    *      of the functions below.
+    *    * The code below is threadsafe since the reference count is
+    *      stored in an atomic<int> var.
+    */
 #define INTRUSIVE_PTR_HELPER(cls) \
-   inline void intrusive_ptr_add_ref(cls * r) \
+   inline void intrusive_ptr_add_ref(const cls * r) \
    { r->addref(); } \
    \
-   inline void intrusive_ptr_release(cls * r)\
-   { if (!r->removeref()) { delete r; } }
+   inline void intrusive_ptr_release(const cls * r)\
+   { if (!r->removeref()) { boost::checked_delete (r); } }
 
-
-
+   //========================================================================
 }
 
 #endif
