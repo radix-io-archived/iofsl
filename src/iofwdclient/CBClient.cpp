183f6b4583606913cec4d84acbc75212113ad606
Jason Cope <copej@mcs.anl.gov>
2011-02-18 09:32:57 -0600

    updated memory mgmt in the CBClient
    - created a wrapper object that stores the user cb,
        an intrusive_ptr to the client sm, and creates
        a wrapped callback
    - wrapped callback invokes the user call back and frees
        the memory allocated for the wrapper
    - when the wrapper is deleted, the ref count will dec
        and cleanup the SM

--- src/iofwdclient/CBClient.cpp
+++ src/iofwdclient/CBClient.cpp
@@ -22,24 +22,39 @@ namespace iofwdclient
    {
    }
 
+   void CBClient::cbWrapper(CBSMWrapper * cbsm,
+           zoidfs::zoidfs_comp_mask_t mask,
+           const iofwdevent::CBException & cbexception)
+   {
+       fprintf(stderr, "%s:%i callback wrapper invoked, mask = %i\n",
+               __func__, __LINE__, mask);
+
+       /* call the original cb */
+       cbsm->call(mask, cbexception);
+
+       /* delete the wrapper... trigger ref count dec on SM */
+       delete cbsm;
+   }
+
    void CBClient::cbgetattr(const IOFWDClientCB & cb,
          int * ret,
          const zoidfs_handle_t * handle,
          zoidfs_attr_t * attr,
          zoidfs_op_hint_t * op_hint)
    {
+       /* create the empty wrapper */
+       CBSMWrapper * cbsm = new CBSMWrapper(cb);
+
        /* create the state machine */
        iofwdclient::clientsm::GetAttrClientSM * sm =
-           new iofwdclient::clientsm::GetAttrClientSM(*smm_, poll_, cb, ret,
-                   handle, attr, op_hint);
+           new iofwdclient::clientsm::GetAttrClientSM(*smm_, poll_,
+                   cbsm->getWCB(), ret, handle, attr, op_hint);
+      
+       /* add the sm to the cb wrapper */ 
+       cbsm->set(sm);
 
+       /* execute the sm */
        sm->execute();
-
-       /* submit the state machine to the SMManager */
-       //smm_->schedule(sm);
-
-       /* TODO where are we tracking the state machines */
-       temp_smclient_queue_.push(sm);
    }
 
    //========================================================================
