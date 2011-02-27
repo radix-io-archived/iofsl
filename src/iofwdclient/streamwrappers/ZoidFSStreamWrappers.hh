3e5bc8497c0fddadb27759938c9d814052ee556b
Jason Cope <copej@mcs.anl.gov>
2011-02-27 15:49:19 -0600

    add zoidfs stream wrappers header

---
+++ src/iofwdclient/streamwrappers/ZoidFSStreamWrappers.hh
@@ -0,0 +1,7 @@
+#ifndef IOFWDCLIENT_STREAMWRAPPERS_ZOIDFSSTREAMWRAPPERS_HH
+#define IOFWDCLIENT_STREAMWRAPPERS_ZOIDFSSTREAMWRAPPERS_HH
+
+/* include for the streams */
+#include "iofwdclient/streamwrappers/GetAttrStreams.hh"
+
+#endif
