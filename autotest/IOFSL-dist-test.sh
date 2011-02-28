114bba404149da5275d985d6e024e4a87f48c10a
Rico D'Amore <rjdamore@lanl.gov>
2011-02-28 10:46:19 -0700

    Added two options to IOFSL-test-dist.sh
    Added the -b(branch) and -n(number of commits) options to
    IOFSL-dist-test.sh
    See autotest/README for details

--- autotest/IOFSL-dist-test.sh
+++ autotest/IOFSL-dist-test.sh
@@ -3,6 +3,35 @@
 # dependencies are located through ../scripts/configoption.${HOSTNAME}. 
 # After installation and construction the script runs a "distcheck" on the build.  
 # test reports are mailed to io-fwd-discuss@lists.mcs.anl.gov
+# This script has two options -n and -b.  -n followed by an integer identifies the number of 
+# commits you wish to test from your present position backwards through history.
+# The -b option followed by a branch name identifies which branch you would wish the test harness to exe# on.  If you wish to use the master branch you can simply leave the -b option out of your script invocation.
+
+checkout_branch(){
+  if
+    [ "$branch" != "before-resourcework-thread" ] && [ "$branch" != "iofsl_vampir" ] && [ "$branch" != "master-merge" ] && [ "$branch" != "clientrpc" ] && [ "$branch" != "paper-freeze" ] && [ "$branch" != "resourcework" ] && [ "$branch" != "tranform" ] 
+  then 
+    :
+  else
+    git checkout $branch
+  fi
+}
+
+while getopts "n:b:" opt; do
+  case $opt in
+    n)
+        num_tests="$OPTARG"
+	echo "Running tests on $OPTARG commits"
+        ;;
+    b) 
+        branch="$OPTARG"
+	checkout_branch
+	;;
+    [?])
+	echo "invalid option! only -n followed by an integer and -b followed by a branch name are valid options. See autotest/README for more info. Thanks."
+	;;
+  esac
+done
 
 get_user_email(){
   echo "Locating git user email."
@@ -32,7 +61,7 @@ read_commits(){
 execute_tests(){
   hostname=$(hostname)
   cp ~/iofsl/scripts/configoptions.tu-fe1.lanl.gov ~/iofsl/scripts/configoptions.$hostname
-  for ((i=0; i<1; i++))
+  for ((i=0; i<$num_tests; i++))
   do
     destroy_touch
   done
@@ -83,7 +112,8 @@ error_report(){
     awk 'NR<=3' runtest_results.txt >> make_error_report.txt; awk '/Error/{c=5}c&&c--' runtest_results.txt >> make_error_report.txt; tail -5 runtest_results.txt >> make_error_report.txt
     echo "=========================================================" >> make_error_report.txt
   comm=${commit:0:7}
-  echo | mutt -c drieskimpe@gmail.com -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@lanl.gov
+  echo | mutt -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@gmail.com
+  #echo | mutt -c drieskimpe@gmail.com -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@lanl.gov
   else test_report 
   fi
   cat make_error_report.txt >> make_error_report-pile.txt
@@ -138,7 +168,7 @@ server_report(){
   fi
   comm=${commit:0:7}
   echo | mutt -a server_report.txt -s "server report for commit $comm" rjdamore@gmail.com
-  test_report
+  
 }
 
 test_report(){
@@ -147,7 +177,8 @@ test_report(){
   echo "test results for commit $commit" >> test_report.txt
   echo >> test_report.txt
   echo "this commit was finished testing at $(date)" >> test_report.txt
-  awk 'NR<=5' runtest_results.txt >> test_report.txt ; egrep 'Running|PASS|All' runtest_results.txt >> test_report.txt ; awk '/--Run/{c=4}c&&c--' runtest_results.txt >> test_report.txt; tail -5 runtest_results.txt >> test_report.txt
+  which gcc >> test_report.txt 
+  awk 'NR<=6' runtest_results.txt >> test_report.txt ; egrep 'Running|PASS|All' runtest_results.txt >> test_report.txt ; awk '/--Run/{c=4}c&&c--' runtest_results.txt >> test_report.txt; tail -5 runtest_results.txt >> test_report.txt
   echo "==========================================================" >> test_report.txt
   cat test_report.txt >> test_report-pile.txt
   if
@@ -157,7 +188,8 @@ test_report(){
       echo "$commit" >> autotest/tested_commits.txt
   fi
   comm=${commit:0:7}
-  echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
+  echo | mutt -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
+  #echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
   #echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@lanl.gov
   #echo | mutt -c dkimpe@mcs.anl.gov -a test_report.txt -s "test report for commit $comm PASS" io-fwd-discuss@lists.mcs.anl.gov 
   edit_files  
@@ -175,9 +207,9 @@ edit_files(){
       echo "$commit" >> autotest/tested_commits.txt
   fi
   rm -f ~/iofsl/runtest_results.txt
-  #chmod -R 777 iofwd-0.1
-  #rm -rf iofwd-0.1
-  #rm -rf iofwd-0.1.tar.gz
+  chmod -R 777 iofwd-0.1
+  rm -rf iofwd-0.1
+  rm -rf iofwd-0.1.tar.gz
   make clean
   make distclean
   git checkout master
