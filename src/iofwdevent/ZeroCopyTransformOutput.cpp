f59f10e56559c6ee04b43fcb55d275a373d14ce3
Benjamin Welton <welton@mcs.anl.gov>
2011-03-02 14:29:06 -0600

    Modified ZeroCopyTransformOutput to remove work units and added proper encode/decode unit test for ZeroCopyTransformOutput.

--- src/iofwdevent/ZeroCopyTransformOutput.cpp
+++ src/iofwdevent/ZeroCopyTransformOutput.cpp
@@ -12,12 +12,20 @@ namespace iofwdevent {
                                                  GenericTransform * transform,
                                                  size_t len)
   {
-    this->internalMemPtr = new char[len];
-    this->internalMem = new ZeroCopyMemoryOutput ((void *)internalMemPtr, len); 
-    this->mem = out;
+    char * internalMemBuf;
+    if (len > 0)
+    {
+      internalMemBuf = new char[len];
+    }
+    else
+    {
+      internalMemBuf = new char[1000];
+    }
+    internalBuf = new ZeroCopyMemoryOutput ((void *)internalMemBuf, len); 
+
+    this->outStream = out;
     this->transform = transform;
   }
-
   /**
    * Returns a ptr to the transformed output data.
    * 
@@ -29,168 +37,185 @@ namespace iofwdevent {
   Handle ZeroCopyTransformOutput::write ( void ** ptr, size_t * size, 
                                           const CBType & cb, size_t suggested)
   {
-    /* If there is any space left in the internal memory, use the remainder 
-       (or up to suggested size). */
-    if (this->internalMem->spaceRemaining() > 0)
+    
+    /* Create a null callback for reading of transformStorage (non blocking) */
+    CBType nullCB = boost::bind(&ZeroCopyTransformOutput::nullCB,
+                                boost::ref(*this), _1);
+
+    /* Check the Internal Memory Buffer. If there is any free space return 
+       the free space in the buffer. */    
+    if (internalBuf->spaceRemaining() > 0)
+      return internalBuf->write(ptr, size, cb, suggested);
+    
+    /* If there is no space remaining, flush internal buffer and proceed 
+       to getting the memory location for the output stream (this->outStream) 
+     */
+    internalBuf->flush(nullCB);
+
+    return getWriteLoc (ptr, size, cb, suggested);
+  }
+
+  /** 
+   * Makes write call to output stream (this is a blocking call) this function 
+   * sets up a call back to transformState when this operation is complete.
+   *
+   * @param ptr           Pointer to the transformed output data
+   * @param size          Pointer to the size of data in ptr 
+   * @param cb            Callback
+   * @param suggested     Suggested size of the write 
+   */
+  Handle ZeroCopyTransformOutput::getWriteLoc (void ** ptr, size_t * size, 
+                                          const CBType & cb, size_t suggested)
+  {
+    /* Create Pointers to store write ptr location for outpur stream */
+    void ** writeLoc = new void * [1];
+    size_t * writeSize = new size_t[1];
+    
+    /* Set up callback for transformation state */
+    CBType transCB = boost::bind(&ZeroCopyTransformOutput::transformState,
+                                 boost::ref(*this), _1, ptr, size, cb, 
+                                 suggested, writeLoc, writeSize);
+
+    /* Run blocking write call to output stream */
+    return outStream->write(writeLoc, writeSize, transCB, suggested);
+  }
+  
+  /** 
+   * Transforms user write then write the resulting data to the output 
+   * stream.
+   *
+   * @param ptr           Pointer to the transformed output data
+   * @param size          Pointer to the size of data in ptr 
+   * @param cb            Callback
+   * @param suggested     Suggested size of the write 
+   * @param writeLoc      Location of the output stream which can be written
+   * @param writeSize     Size availible for writing to the output stream.
+   */
+  void ZeroCopyTransformOutput::transformState (CBException e,
+                                                void ** ptr, 
+                                                size_t * size, 
+                                                const CBType & cb, 
+                                                size_t suggested,
+                                                void ** writeLoc,
+                                                size_t * writeSize)
+  {
+    e.check();
+    int outState = 0;
+    Handle x;
+    /* Preform the transform */  
+    outState = doTransform (writeLoc, writeSize, false);
+    
+    /* If we are out of input buffer, reset internal buffer and return the 
+       write pointer to the user */
+    if (outState == SUPPLY_INBUF)
     {
-      return this->internalMem->write(ptr, size, cb, suggested);
+      internalBuf->reset();
+      x = internalBuf->write(ptr, size, cb, suggested);
+    } else if (outState == CONSUME_OUTBUF)
+    /* If we are out of output buffer, repeat this run */
+    {
+      getWriteLoc(ptr, size, cb, suggested);
     }
-    /* Else this is a blocking write */
-    else
-      return this->blockingWrite(ptr, size, cb, suggested);
+    
+    /* We should never get here, transformation should not be signaled as done
+       at this location */
+    assert (outState != 0);
   }
 
   /**
-   * Internal function that actually performs the write request. Assume that
-   * this function will block (it wont always but assume that it can/will)
+   * Preform the transform on input data to output data 
+   * 
+   * @param writeLoc      Location to write transformed data
+   * @param writeSize     Size of the write location 
+   * @param flushFlag     Flush flag for the stream 
    */
-  Handle ZeroCopyTransformOutput::blockingWrite (void ** ptr, size_t * size,
-                                                 const CBType & cb, 
-                                                 size_t suggested)
+  int ZeroCopyTransformOutput::doTransform ( void ** writeLoc, 
+                                              size_t * writeSize,
+                                              bool flushFlag)
   {
-    /* Create the work unit for this callback */
-    ZeroCopyTransformWriteWU * wu = new ZeroCopyTransformWriteWU (ptr, size, 
-                                        cb, suggested);
 
-    /* Bind internal callback (not client callback) */
-    CBType writeCB = boost::bind(&ZeroCopyTransformOutput::blockingWriteCB,
-                                 boost::ref(*this), _1, wu);
+    Handle ret;
+    size_t transOut = 0;
+    int outState = 0;
+    /* Get the memory locations for the internal buffer */
+    size_t streamLen = internalBuf->getOffset();
+    void * streamData  = internalBuf->getMemPtr();
+    /* Create a null callback for reading of transformStorage (non blocking) */
+    CBType nullCB = boost::bind(&ZeroCopyTransformOutput::nullCB,
+                                boost::ref(*this), _1);
 
-    /* Bind the function for workqueue */
-    boundFunc f = boost::bind(&ZeroCopyTransformOutput::flush, 
-                           boost::ref(*this), writeCB);
+    /* preform the transform */
+    transform->transform ((const void *const) streamData, streamLen,
+                          *writeLoc, *writeSize, &transOut,
+                          &outState, flushFlag);
 
-    /* Lock the callback queue */
-    boost::mutex::scoped_lock lock(this->lock_);
+    /* return unused output stream space */
+    ret = outStream->rewindOutput(*writeSize - transOut, nullCB);
 
-    /* Push the callback onto the queue */
-    this->writeCBQueue.push_back(f);
-    
-    return (Handle)0;
+    return outState;
   }
 
-  void ZeroCopyTransformOutput::blockingWriteCB(CBException e, 
-                                                ZeroCopyTransformWriteWU * wu)
-  {
-    e.check();
-
-    /* If there is any space left in the internal memory, use the remainder 
-       (or up to suggested size). */
-    if (this->internalMem->spaceRemaining() > 0)
-    {
-      this->internalMem->write(wu->ptr_, wu->size_, wu->cb_, wu->suggested_);
-    }
-    else
-      assert(1==0);
-    delete wu;
-  }
-
-  /* Flush internal buffers, if any */
+  /**
+   * Flush the internal buffers, if any 
+   *  
+   * @param cb        Callback for function
+   */
   Handle ZeroCopyTransformOutput::flush (const CBType & cb)
   {
-    CBType flushCB;
-    Handle x;
-    size_t curSize = this->internalMem->getTotalLen();
-
-    /* If there is nothing to flush */
-    if (curSize == 0)
-    {
-      cb(*(new CBException()));
-      return (Handle) 0;
-    }
-
-    /* Convert the internalMem ZeroCopyOutput data type to ZeroCopyInput so
-       that it can be read */
-    ZeroCopyMemoryInput * internal = this->convertToMemoryInput
-                                                          (this->internalMem);
-
-    /* Create the workunit for the flush */
-    ZeroCopyTransformFlushWU * wu = new ZeroCopyTransformFlushWU (cb);
-
-    /* Create the callback */
-    flushCB = boost::bind(&ZeroCopyTransformOutput::internalReadCB,
-                          boost::ref(*this), _1, wu);
-
-    /* Make the call to the internal read */
-    x = internal->read((const void **) &wu->inData_, &wu->inLen_, flushCB, curSize);
-
-    /* This is an assumption that the size of the data read is equal to the
-       size of the stream currently (this assumption may need to change) */
-    assert(wu->inLen_ == curSize);
-
-    return (Handle) 0;
-  }
-
-  /* No Callback */
-  void ZeroCopyTransformOutput::nullCB (CBException e)
-  { 
-    e.check();
-  }
-
-  /* Finale Flush Stage */
-  void ZeroCopyTransformOutput::doTransform (ZeroCopyTransformFlushWU * flushWu)
-  {
-    void ** outData_ = new void *[1];
-    size_t outLen_;
-    size_t outputSize_;
-    int outState_;
-    bool flushFlag_ = true;
-
-    /* Get the write location */
-    CBType nullCB = boost::bind(&ZeroCopyTransformOutput::nullCB, 
-                                  boost::ref(*this), _1 );
+    void ** writeLoc = new void * [1];
+    size_t * writeSize = new size_t[1];
+    
+    size_t streamLen = internalBuf->getOffset();
+    /* Set up callback for transformation state */
+    CBType transCB = boost::bind(&ZeroCopyTransformOutput::flushTransform,
+                                 boost::ref(*this), cb, writeLoc, writeSize);
   
-    /* Get location to write to */
-    this->mem->write(outData_, &outLen_, nullCB, flushWu->inLen_);
+    return outStream->write(writeLoc, writeSize, transCB, streamLen);
+
+  }
+
+  /**
+   * Handles getting write pointer from output stream 
+   * 
+   * @param cb       User Callback
+   */
+  void ZeroCopyTransformOutput::flushTransform (const CBType & cb,
+                                                void ** writeLoc, 
+                                                size_t * writeSize)
+  {
+    int outState = 0;
+    /* Create a null callback for flushing the internal memory stream */
+    CBType nullCB = boost::bind(&ZeroCopyTransformOutput::nullCB,
+                                boost::ref(*this), _1);
+
+    /* Flush the internal buffer */
+    internalBuf->flush(nullCB);
 
     /* Preform the transform */
-    this->transform->transform ((const void* const) flushWu->inData_, 
-                                flushWu->inLen_, outData_, 
-                                outLen_, &outputSize_, 
-                                &outState_, flushFlag_);
+    outState = doTransform ( writeLoc, writeSize, true);
 
-    /* If all the output from write was not used, return unused portion */
-    if (outputSize_ != outLen_)
-      this->mem->rewindOutput(outLen_ - outputSize_, nullCB);
-
-    this->internalMem->reset();
-
-    flushWu->cb_(*(new CBException()));
-    
+    /* If there is output that has not been consumed, preform another flush */
+    if (outState == CONSUME_OUTBUF)
+    {
+      flush(cb);
+    }
+    /* Else flush the output stream and call the users callback */
+    else
+    {
+      outStream->flush(cb);
+    }
   }
-  /* Flush stage one completion */
-  void ZeroCopyTransformOutput::internalReadCB(CBException e, ZeroCopyTransformFlushWU * wu)
-  {
-    e.check();
-    /* Lock the work unit queue (may need a differnt lock for this) */
-    boost::mutex::scoped_lock lock(this->lock_);    
-
-    /* Bind the function for workqueue */
-    boundFunc f = boost::bind(&ZeroCopyTransformOutput::doTransform, 
-                           boost::ref(*this), wu);
-
-    this->writeCBQueue.push_back(f);
-  }
-
-  /* Rewinds the write stream */
+  /**
+   * Rewinds the write stream 
+   *
+   * @param size    Size of the rewind 
+   * @param cb      Callback
+   */
   Handle ZeroCopyTransformOutput::rewindOutput (size_t size, 
                                                 const CBType & cb)
   {
-    return this->internalMem->rewindOutput(size, cb);
+    return internalBuf->rewindOutput(size, cb);
   }
 
-  /**
-   * Converts ZeroCopyMemoryOutput -> ZeroCopyMemoryInput. This is
-   * used for changing the direction of the internal ZeroCopyMemory stream 
-   * from write mode (output) to read mode (input) for the memory region 
-   * stored in ZeroCopyMemoryOutput.
-   */
-  ZeroCopyMemoryInput * ZeroCopyTransformOutput::convertToMemoryInput(
-                                              ZeroCopyMemoryOutput * x)
-  {
-    void * ptr = x->getMemPtr();
-    ZeroCopyMemoryInput * v = new ZeroCopyMemoryInput ( ptr, x->getTotalLen());
-    return v;
-  }
 
 }
--- src/iofwdevent/ZeroCopyTransformOutput.hh
+++ src/iofwdevent/ZeroCopyTransformOutput.hh
@@ -21,86 +21,32 @@
 using namespace iofwdutil;
 using namespace boost;
 namespace iofwdevent {
-    class ZeroCopyTransformWriteWU {
-      public:
-        ZeroCopyTransformWriteWU(void ** ptr, size_t * size, 
-                                 const CBType & cb, size_t suggested) :
-            ptr_(ptr), size_(size), cb_(cb), suggested_(suggested)
-        {
-        }
-        void ** ptr_;
-        size_t * size_;
-        const CBType cb_;
-        size_t suggested_;
-        ZeroCopyMemoryOutput * internalMem_;
-    };
-
-    class ZeroCopyTransformFlushWU {
-      public:
-        ZeroCopyTransformFlushWU (const CBType & cb) :
-          cb_(cb)
-        {
-        }
-        void * inData_;
-        size_t inLen_;
-        const CBType cb_;
-    };
-
-    class ZeroCopyTransformWU {
-      public:
-        ZeroCopyTransformWU (void * outData, size_t outLen, size_t outputSize,
-                             int outState, bool flushFlag, const CBType & cb ) :
-          outData_(outData), outLen_(outLen), outputSize_(outputSize), 
-          outState_(outState), flushFlag_(flushFlag), cb_(cb)
-        {
-        }
-        void * outData_;
-        size_t outLen_;
-        size_t outputSize_;
-        int outState_;
-        bool flushFlag_;
-        const CBType cb_;
-    };
 
     class ZeroCopyTransformOutput : public ZeroCopyOutputStream {
       typedef iofwdutil::transform::GenericTransform GenericTransform;
-      typedef boost::function< void() > boundFunc;
 
       protected:
-        ZeroCopyOutputStream * mem; /*< Stores input stream from which to transform */
+        ZeroCopyOutputStream * outStream; /*< Stores input stream from which to transform */
         GenericTransform * transform; /*< Stores transform information */
-        size_t memSize;          /*< Stores the size of the memory location */
-        size_t pos;              /*< Stores current pointer position inside mem */
-        ZeroCopyMemoryOutput * internalMem; /* Stores the internal memory stream
+        ZeroCopyMemoryOutput * internalBuf; /* Stores the internal memory stream
                                              for this class */
-        char * internalMemPtr;
-        mutable boost::mutex lock_;
-        std::deque< boundFunc > writeCBQueue; 
-        std::deque< boundFunc > flushCBQueue; 
-
+        static const int SUPPLY_INBUF = iofwdutil::transform::SUPPLY_INBUF;
+        static const int TRANSFORM_DONE = iofwdutil::transform::TRANSFORM_DONE;
+        static const int CONSUME_OUTBUF = iofwdutil::transform::CONSUME_OUTBUF;
       public:
-        /* Constructor for InputMemoryZeroCopy. */
-        ZeroCopyTransformOutput  (ZeroCopyOutputStream * , GenericTransform * , size_t);
-
-        /* Cancel operation (not used since this class does not block) */
-        void cancel (Handle x) { x = (Handle) 0;}; 
-
-        /* Returns pointer to memory area where a write can take place */
-        Handle write (void **, size_t *, const CBType & , size_t);
-
-        /* Rewinds the write stream */
-        Handle rewindOutput (size_t, const CBType &);
-
-        /* Flush internal buffers, if any */
+        ZeroCopyTransformOutput(ZeroCopyOutputStream * ,
+                                GenericTransform * , size_t );
+        Handle write ( void ** , size_t * , const CBType & , size_t );
+        Handle getWriteLoc (void **, size_t *, const CBType & , size_t );
+        void transformState (CBException ,void ** ,size_t * ,const CBType & ,
+                        size_t , void ** , size_t * );
+        int doTransform ( void ** ,  size_t * , bool );
         Handle flush (const CBType & );
-
-        /* Convert between a ZeroCopyMemoryOutput and a ZeroCopyMemoryInput */
-        ZeroCopyMemoryInput * convertToMemoryInput(ZeroCopyMemoryOutput *);
-        Handle blockingWrite (void ** , size_t * , const CBType &, size_t);
-        void blockingWriteCB(CBException, ZeroCopyTransformWriteWU * );
-        void nullCB (CBException );
-        void doTransform (ZeroCopyTransformFlushWU * );
-        void internalReadCB(CBException , ZeroCopyTransformFlushWU * );
+        void flushTransform (const CBType &, void **, size_t *);
+        Handle rewindOutput (size_t , const CBType & );
+        void cancel (Handle x) { x = (Handle) 0; }; 
+   
+        void nullCB (CBException e) { e.check();};
     };
     //void blockingWriteCB(CBException );
 }
