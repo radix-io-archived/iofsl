1d754499185045ca0875777b47277f6d1c53e379
Benjamin Welton <welton@mcs.anl.gov>
2011-03-20 00:50:37 -0500

    Changed IOFSLRPC GetAttrRequest to use macro's for generation of Decode/Encode functions and the related process() function calls

--- src/iofwd/rpcfrontend/IOFSLRPCGetAttrRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCGetAttrRequest.cpp
@@ -1,70 +1,48 @@
 #include "iofwd/rpcfrontend/IOFSLRPCGetAttrRequest.hh"
 #include "iofwdutil/tools.hh"
-
+#include "IOFSLRPCGenProcess.hh"
 namespace iofwd
 {
    namespace rpcfrontend
    {
 
-void IOFSLRPCGetAttrRequest::decode()
-{
-    process(dec_, handle_);
-    process(dec_, attr_);
-    zoidfs::hints::zoidfs_hint_create(&op_hint_);
-    decodeOpHint(&op_hint_);
-}
+      RPC_GENPROCESS (IOFSLRPCGetAttrRequest, ((&)(handle))
+                                              ((&)(attr)),
+                                              (()(returnCode))
+                                              ((*)(attr_enc_)))
 
-const IOFSLRPCGetAttrRequest::ReqParam & IOFSLRPCGetAttrRequest::decodeParam()
-{
-    /* decode the rpc input params */
-    decodeRPCInput();
 
-    /* setup the req param */
-    param_.handle = &handle_;
-    param_.attr = &attr_;
-    param_.op_hint = &op_hint_;
 
-    /* return a ref to the req param */
-    return param_;
-}
+      void IOFSLRPCGetAttrRequest::reply(const CBType & UNUSED(cb),
+              const zoidfs::zoidfs_attr_t * attr)
+      {
+          /* verify the args are OK */
+          ASSERT(getReturnCode() != zoidfs::ZFS_OK || attr);
 
-void IOFSLRPCGetAttrRequest::encode()
-{
-    /* process the output */
-    process(enc_, getReturnCode());
-    process(enc_, const_cast<const zoidfs::zoidfs_attr_t &>(*attr_enc_));
-}
+          /* store ptr to the attr */
+          attr_enc_ = const_cast<zoidfs::zoidfs_attr_t *>(attr);
 
-void IOFSLRPCGetAttrRequest::reply(const CBType & UNUSED(cb),
-        const zoidfs::zoidfs_attr_t * attr)
-{
-    /* verify the args are OK */
-    ASSERT(getReturnCode() != zoidfs::ZFS_OK || attr);
+          /* encode */
+          encodeRPCOutput();
 
-    /* store ptr to the attr */
-    attr_enc_ = const_cast<zoidfs::zoidfs_attr_t *>(attr);
+          /* invoke the callback */
+          //cb();
+      }
 
-    /* encode */
-    encodeRPCOutput();
+      IOFSLRPCGetAttrRequest::~IOFSLRPCGetAttrRequest()
+      {
+         zoidfs::hints::zoidfs_hint_free(&op_hint_);
+      }
 
-    /* invoke the callback */
-    //cb();
-}
+      size_t IOFSLRPCGetAttrRequest::rpcEncodedInputDataSize()
+      {
+          return 0;
+      }
 
-IOFSLRPCGetAttrRequest::~IOFSLRPCGetAttrRequest()
-{
-   zoidfs::hints::zoidfs_hint_free(&op_hint_);
-}
-
-size_t IOFSLRPCGetAttrRequest::rpcEncodedInputDataSize()
-{
-    return 0;
-}
-
-size_t IOFSLRPCGetAttrRequest::rpcEncodedOutputDataSize()
-{
-    return 0;
-}
+      size_t IOFSLRPCGetAttrRequest::rpcEncodedOutputDataSize()
+      {
+          return 0;
+      }
 
    }
 }
