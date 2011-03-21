8f6333efce4e73e1b599913e462789cbeeae4bb3
Benjamin Welton <welton@mcs.anl.gov>
2011-03-21 14:15:01 -0500

    Added IOFSLRPCReadRequest into build system.

--- src/iofwd/rpcfrontend/IOFSLRPCReadRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCReadRequest.hh
@@ -1,43 +1,35 @@
-#ifndef IOFWD_RPCFRONTEND_IOFSLRPCREADLINKREQUEST_HH
-#define IOFWD_RPCFRONTEND_IOFSLRPCREADLINKREQUEST_HH
+#ifndef IOFWD_RPCFRONTEND_IOFSLRPCREADREQUEST_HH
+#define IOFWD_RPCFRONTEND_IOFSLRPCREADREQUEST_HH
 
 #include "zoidfs/util/zoidfs-wrapped.hh"
 #include "zoidfs/zoidfs.h"
 
 #include "iofwd/ReadRequest.hh"
 #include "iofwd/rpcfrontend/IOFSLRPCRequest.hh"
-
+#include "iofwdutil/mm/BMIMemoryManager.hh"
 namespace iofwd
 {
    namespace rpcfrontend
    {
-//      zoidfs::zoidfs_handle_t * handle;
-//      size_t mem_count;
-//      void ** mem_starts;
-//      size_t * mem_sizes;
-//      size_t file_count;
-//      zoidfs::zoidfs_file_ofs_t * file_starts;
-//      zoidfs::zoidfs_file_ofs_t * file_sizes;
-//      size_t pipeline_size;
-//      zoidfs::zoidfs_op_hint_t * op_hint;
 
-//      typedef zoidfs::zoidfs_dirent_t zoidfs_dirent_t;
-//      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
-//      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
-//      typedef zoidfs::zoidfs_dirent_cookie_t zoidfs_dirent_cookie_t;
-//      typedef zoidfs::zoidfs_cache_hint_t zoidfs_cache_hint_t;
-//      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
+      typedef zoidfs::zoidfs_dirent_t zoidfs_dirent_t;
+      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
+      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
+      typedef zoidfs::zoidfs_dirent_cookie_t zoidfs_dirent_cookie_t;
+      typedef zoidfs::zoidfs_file_ofs_t zoidfs_file_ofs_t;
 
-//      ENCODERSTRUCT (IOFSLRPCReadDirDec, ((zoidfs_handle_t)(handle)) 
-//                                        ((zoidfs_dirent_cookie_t)(cookie))
-//                                        ((uint32_t)(entry_count))              
-//                                        ((zoidfs_dirent_t)(entries))
-//                                        ((uint32_t)(flags)))
+      ENCODERSTRUCT (IOFSLRPCReadDec, ((zoidfs_handle_t)(handle)) 
+                                        ((size_t)(mem_count))
+                                        ((void**)(mem_starts))              
+                                        ((size_t*)(mem_sizes))
+                                        ((size_t)(file_count))
+                                        ((zoidfs_file_ofs_t)(file_starts))
+                                        ((zoidfs_file_ofs_t)(file_sizes))
+                                        ((size_t)(pipeline_size)))
 
-//      ENCODERSTRUCT (IOFSLRPCReadDirEnc, ((int)(returnCode))
-//                                         ((uint32_t)(entry_count))
-//                                         ((zoidfs_dirent_t)(entries))
-//                                         ((zoidfs_cache_hint_t)(cache)))
+      ENCODERSTRUCT (IOFSLRPCReadEnc, ((int)(returnCode))
+                                      ((zoidfs_file_ofs_t)(file_starts))
+                                      ((zoidfs_file_ofs_t)(file_sizes)))
 
       /* decoded hint values */
       bool op_hint_pipeline_enabled;
@@ -52,43 +44,47 @@ namespace iofwd
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
                   IOFSLRPCRequest(in, out),
-                  ReadRequest(opid),
-                  attr_enc_(NULL)
+                  ReadRequest(opid)
               {
               }
             
               virtual ~IOFSLRPCReadRequest();
 
               /* encode and decode helpers for RPC data */
-              virtual void decode();
-              virtual void encode();
+              void decode();
+              void encode();
 
-              virtual ReqParam & decodeParam () = 0;
+              ReqParam & decodeParam ();
 
-              virtual void reply(const CBType & cb) = 0;
+              void reply(const CBType & cb);
 
               // for normal mode
-              virtual void sendBuffers(const iofwdevent::CBType & cb, RetrievedBuffer * rb) = 0;
+              void sendBuffers(const iofwdevent::CBType & cb, 
+                               RetrievedBuffer * rb);
 
               // for pipeline mode
-              virtual void sendPipelineBufferCB(const iofwdevent::CBType cb, RetrievedBuffer * rb, size_t size) = 0;
+              void sendPipelineBufferCB(const iofwdevent::CBType cb, 
+                                        RetrievedBuffer * rb, size_t size);
 
-              virtual void initRequestParams(ReqParam & p, void * bufferMem) = 0;
+              void initRequestParams(ReqParam & p, void * bufferMem);
 
-              virtual void allocateBuffer(iofwdevent::CBType cb, RetrievedBuffer * rb) = 0;
-
-              virtual void releaseBuffer(RetrievedBuffer * rb) = 0;
-                      
+              void allocateBuffer(iofwdevent::CBType cb, RetrievedBuffer * rb);
+              void releaseBuffer(RetrievedBuffer * rb);
+              void writeBuffer(void * buff, size_t size, bool flush);
           protected:
               /* data size helpers for this request */ 
               virtual size_t rpcEncodedInputDataSize(); 
               virtual size_t rpcEncodedOutputDataSize();
 
               ReqParam param_;
-              zoidfs::zoidfs_handle_t handle_;
-              zoidfs::zoidfs_attr_t attr_;
+
               zoidfs::zoidfs_op_hint_t op_hint_;
-              zoidfs::zoidfs_attr_t * attr_enc_;
+
+              // @TODO: This should not be specified here. however its required
+              //        to use the memory manager (BMIMemoryManager)
+              iofwdutil::bmi::BMIAddr * addr_;
+              IOFSLRPCReadEnc enc_struct;
+              IOFSLRPCReadDec dec_struct;
       };
 
    }
