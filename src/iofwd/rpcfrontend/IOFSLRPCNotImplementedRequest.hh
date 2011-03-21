738f0c8376cec9fc526eb3b2624fc173caec3b35
Benjamin Welton <welton@mcs.anl.gov>
2011-03-21 17:18:06 -0500

    Added RPCNotImplemented Request

---
+++ src/iofwd/rpcfrontend/IOFSLRPCNotImplementedRequest.hh
@@ -0,0 +1,51 @@
+#ifndef IOFWD_RPCFRONTEND_IOFSLRPCNOTIMPLEMENTED_HH
+#define IOFWD_RPCFRONTEND_IOFSLRPCNOTIMPLEMENTED_HH
+
+/* NO CPP FILE */
+
+#include "zoidfs/util/zoidfs-wrapped.hh"
+#include "zoidfs/zoidfs.h"
+
+#include "iofwd/NotImplementedRequest.hh"
+#include "iofwd/rpcfrontend/IOFSLRPCRequest.hh"
+#include "iofwdevent/CBType.hh"
+namespace iofwd
+{
+   namespace rpcfrontend
+   {
+      typedef struct 
+      {} ReqParam;
+      
+      class IOFSLRPCNotImplementedRequest :
+          public IOFSLRPCRequest,
+          public NotImplementedRequest
+      {
+          public:
+              IOFSLRPCNotImplementedRequest(int opid,
+                      iofwdevent::ZeroCopyInputStream * in,
+                      iofwdevent::ZeroCopyOutputStream * out) :
+                  IOFSLRPCRequest(in, out),
+                  NotImplementedRequest(opid)
+              {
+              }
+            
+              virtual ~IOFSLRPCNotImplementedRequest();
+
+              /* encode and decode helpers for RPC data */
+              void decode() {};
+              void encode() { process(enc_, getReturnCode()); };
+
+              /* request processing */
+              const ReqParam & decodeParam() {return param_;};
+              void reply (const CBType & cb);
+          protected:
+              /* data size helpers for this request */ 
+              virtual size_t rpcEncodedInputDataSize()  { return 0; }; 
+              virtual size_t rpcEncodedOutputDataSize() { return 0; };
+
+              ReqParam param_;
+      };
+   }
+}
+
+#endif
