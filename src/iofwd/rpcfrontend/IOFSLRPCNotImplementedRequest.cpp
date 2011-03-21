3b5578176a10ceee18873a2ef46f0e69076f0cfd
Benjamin Welton <welton@mcs.anl.gov>
2011-03-21 17:18:06 -0500

    Added RPCNotImplemented Request

---
+++ src/iofwd/rpcfrontend/IOFSLRPCNotImplementedRequest.cpp
@@ -0,0 +1,15 @@
+#include "iofwd/rpcfrontend/IOFSLRPCNotImplementedRequest.hh"
+
+namespace iofwd
+{
+   namespace rpcfrontend
+   {
+        void IOFSLRPCNotImplementedRequest::reply (const CBType & cb)
+        {
+          /* verify the args are OK */
+          ASSERT(getReturnCode() != zoidfs::ZFS_OK);
+          /* encode */
+          encodeRPCOutput();
+        }
+   }
+}
