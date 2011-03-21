34b734821e8a6b3d5a9fe9bb02f60fafdaaf8282
Benjamin Welton <welton@mcs.anl.gov>
2011-03-21 17:48:14 -0500

    Added RPC Rename Request

--- src/iofwd/rpcfrontend/IOFSLRPCRenameRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCRenameRequest.hh
@@ -3,14 +3,34 @@
 
 #include "zoidfs/util/zoidfs-wrapped.hh"
 #include "zoidfs/zoidfs.h"
-
+#include "encoder/EncoderStruct.hh"
+#include "encoder/EncoderString.hh"
 #include "iofwd/RenameRequest.hh"
 #include "iofwd/rpcfrontend/IOFSLRPCRequest.hh"
+#include "encoder/EncoderWrappers.hh"
+using namespace encoder;
 
 namespace iofwd
 {
    namespace rpcfrontend
    {
+
+      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
+      typedef zoidfs::zoidfs_sattr_t zoidfs_sattr_t;
+      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
+      typedef zoidfs::zoidfs_cache_hint_t zoidfs_cache_hint_t;
+
+      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
+      ENCODERSTRUCT (IOFSLRPCRenameDec, ((EncoderString)(from_full_path)) 
+                                        ((zoidfs_handle_t)(from_parent_handle))
+                                        ((EncoderString)(from_component_name))              
+                                        ((EncoderString)(to_full_path))
+                                        ((zoidfs_handle_t)(to_parent_handle))
+                                        ((EncoderString)(to_component_name)))
+
+      ENCODERSTRUCT (IOFSLRPCRenameEnc, ((int)(returnCode))
+                                        ((zoidfs_cache_hint_t)(from_parent_hint))
+                                        ((zoidfs_cache_hint_t)(to_parent_hint)))
 
       class IOFSLRPCRenameRequest :
           public IOFSLRPCRequest,
@@ -21,41 +41,35 @@ namespace iofwd
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
                   IOFSLRPCRequest(in, out),
-                  RenameRequest(opid),
-                  attr_enc_(NULL)
+                  RenameRequest(opid)
               {
               }
-            
-              virtual ~IOFSLRPCRenameRequest();
 
               /* encode and decode helpers for RPC data */
               virtual void decode();
               virtual void encode();
-              /**
-              * Retrieve the request input parameters
-              */
-              virtual const ReqParam & decodeParam ()  = 0;
 
-              /**
-              * Reply with the handle or 0 if an error occurred and the handle does not
-              * need to be transmitted
-              */
+              /* request processing */
+              virtual const ReqParam & decodeParam();
               virtual void reply (const CBType & cb, 
-                        const zoidfs::zoidfs_cache_hint_t * from_parent_hint,
-                        const zoidfs::zoidfs_cache_hint_t * to_parent_hint) = 0;
+                                  const zoidfs::zoidfs_cache_hint_t *
+                                    from_parent_hint_, 
+                                  const zoidfs::zoidfs_cache_hint_t * 
+                                    to_parent_hint_);
+          
+              ~IOFSLRPCRenameRequest();
           protected:
               /* data size helpers for this request */ 
               virtual size_t rpcEncodedInputDataSize(); 
               virtual size_t rpcEncodedOutputDataSize();
 
               ReqParam param_;
-              zoidfs::zoidfs_handle_t handle_;
-              zoidfs::zoidfs_attr_t attr_;
+              IOFSLRPCRenameDec dec_struct;
+              IOFSLRPCRenameEnc enc_struct;
+              zoidfs::zoidfs_cache_hint_t * from_parent_hint;
+              zoidfs::zoidfs_cache_hint_t * to_parent_hint;
               zoidfs::zoidfs_op_hint_t op_hint_;
-              zoidfs::zoidfs_attr_t * attr_enc_;
       };
-
    }
 }
-
 #endif
