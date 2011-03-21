413a69dab371674c0027d67dac4f39f0a47e3995
Benjamin Welton <welton@mcs.anl.gov>
2011-03-21 18:15:27 -0500

    Add RPC SymLink Request

--- src/iofwd/rpcfrontend/IOFSLRPCSymLinkRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCSymLinkRequest.hh
@@ -3,7 +3,8 @@
 
 #include "zoidfs/util/zoidfs-wrapped.hh"
 #include "zoidfs/zoidfs.h"
-
+#include "encoder/EncoderStruct.hh"
+#include "encoder/EncoderString.hh"
 #include "iofwd/SymLinkRequest.hh"
 #include "iofwd/rpcfrontend/IOFSLRPCRequest.hh"
 
@@ -11,7 +12,23 @@ namespace iofwd
 {
    namespace rpcfrontend
    {
+      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
+      typedef zoidfs::zoidfs_sattr_t zoidfs_sattr_t;
+      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
+      typedef zoidfs::zoidfs_cache_hint_t zoidfs_cache_hint_t;
 
+      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
+      ENCODERSTRUCT (IOFSLRPCSymLinkDec, ((EncoderString)(from_full_path)) 
+                                        ((zoidfs_handle_t)(from_parent_handle))
+                                        ((EncoderString)(from_component_name))              
+                                        ((EncoderString)(to_full_path))
+                                        ((zoidfs_handle_t)(to_parent_handle))
+                                        ((EncoderString)(to_component_name))
+                                        ((zoidfs_sattr_t)(sattr)))
+
+      ENCODERSTRUCT (IOFSLRPCSymLinkEnc, ((int)(returnCode))
+                                        ((zoidfs_cache_hint_t)(from_parent_hint))
+                                        ((zoidfs_cache_hint_t)(to_parent_hint)))
       class IOFSLRPCSymLinkRequest :
           public IOFSLRPCRequest,
           public SymLinkRequest
@@ -21,29 +38,28 @@ namespace iofwd
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
                   IOFSLRPCRequest(in, out),
-                  SymLinkRequest(opid),
-                  attr_enc_(NULL)
+                  SymLinkRequest(opid)
               {
               }
             
               virtual ~IOFSLRPCSymLinkRequest();
 
               /* encode and decode helpers for RPC data */
-              virtual void decode();
-              virtual void encode();
+              void decode();
+              void encode();
 
               /**
               * Retrieve the request input parameters
               */
-              virtual const ReqParam & decodeParam ()  = 0;
+              const ReqParam & decodeParam ()  = 0;
 
               /**
               * Reply with the handle or 0 if an error occurred and the handle does not
               * need to be transmitted
               */
-              virtual void reply (const CBType & cb,
+              void reply (const CBType & cb,
                   const zoidfs::zoidfs_cache_hint_t * from_parent_hint,
-                  const zoidfs::zoidfs_cache_hint_t * to_parent_hint) = 0;
+                  const zoidfs::zoidfs_cache_hint_t * to_parent_hint);
 
           protected:
               /* data size helpers for this request */ 
@@ -51,10 +67,11 @@ namespace iofwd
               virtual size_t rpcEncodedOutputDataSize();
 
               ReqParam param_;
-              zoidfs::zoidfs_handle_t handle_;
-              zoidfs::zoidfs_attr_t attr_;
+              IOFSLRPCSymLinkDec dec_struct;
+              IOFSLRPCSymLinkEnc enc_struct;
+              zoidfs::zoidfs_cache_hint_t * from_parent_hint;
+              zoidfs::zoidfs_cache_hint_t * to_parent_hint;
               zoidfs::zoidfs_op_hint_t op_hint_;
-              zoidfs::zoidfs_attr_t * attr_enc_;
       };
 
    }
