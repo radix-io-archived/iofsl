794f5b4fb2b68690d5dc8a0214d3aac518fbd621
Benjamin Welton <welton@mcs.anl.gov>
2011-03-22 01:50:55 -0500

    Client State Machine and Stream macros

---
+++ src/iofwdclient/clientsm/ClientSMMarcos.hh
@@ -0,0 +1,94 @@
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/elem.hpp>
+#include <boost/preprocessor/seq/fold_left.hpp>
+#include <boost/preprocessor/seq/cat.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq/pop_back.hpp>
+                   
+#define CLIENT_GENERATESM(CLASSNAME, OUTSTREAMNAME, NAME)                    \
+   namespace iofwdclient                                                     \
+   {                                                                         \
+       namespace clientsm                                                    \
+       {                                                                     \
+   CLASSNAME::~CLASSNAME()                                                   \
+   {                                                                         \
+   }                                                                         \
+   void CLASSNAME::init(iofwdevent::CBException e)                           \
+   {                                                                         \
+       fprintf(stderr, "%s:%i\n", __func__, __LINE__);                       \
+       e.check();                                                            \
+       setNextMethod(&CLASSNAME::postRPCServerSM);                           \
+   }                                                                         \
+   void CLASSNAME::postRPCServerSM(iofwdevent::CBException e)                \
+   {                                                                         \
+       fprintf(stderr, "%s:%i\n", __func__, __LINE__);                       \
+       e.check();                                                            \
+       server_sm_.reset(new RPCServerSM< CLASSNAME, OUTSTREAMNAME >(smm_,    \
+                  poll_, slots_[BASE_SLOT], NAME, in_, out_));    \
+       slots_.wait(BASE_SLOT, &CLASSNAME::waitRPCServerSM);                  \
+   }                                                                         \
+   void CLASSNAME::waitRPCServerSM(iofwdevent::CBException e)                \
+   {                                                                         \
+       fprintf(stderr, "%s:%i\n", __func__, __LINE__);                       \
+       e.check();                                                            \
+       cb_(zoidfs::ZFS_COMP_DONE, e);                                        \
+   }                                                                         \
+   void CLASSNAME::postSMErrorState(iofwdevent::CBException e)               \
+   {                                                                         \
+       fprintf(stderr, "%s:%i\n", __func__, __LINE__);                       \
+       e.check();                                                            \
+                                                                             \
+   }                                                                         \
+   } }
+                                                        
+#define CLIENT_GENERATEHEADERSM_PARAMS(r,data,elem) CLIENT_GENERATEHEADERSM_S1(elem)
+#define CLIENT_GENERATEHEADERSM_S1(elem) CLIENT_GENERATEHEADERSM_S2(elem),
+#define CLIENT_GENERATEHEADERSM_S2(elem) BOOST_PP_SEQ_FOLD_LEFT(CLIENT_DOFOLD, \
+                                        BOOST_PP_SEQ_HEAD(elem),             \
+                                        BOOST_PP_SEQ_TAIL(elem))
+#define CLIENT_DOFOLD(r,data,elem) data elem
+
+#define CLIENT_GENERATEHEADERSM(CLASSNAME, OUTSTREAMNAME, INSTREAMNAME, PARAMETERS, INPARAMS, OUTPARAMS)          \
+   namespace iofwdclient                                                     \
+   {                                                                         \
+      using namespace streamwrappers;                                        \
+      namespace clientsm                                                     \
+      {                                                                      \
+         class CLASSNAME :                                                   \
+            public sm::SimpleSM< iofwdclient::clientsm::CLASSNAME >          \
+         {                                                                   \
+            public:                                                          \
+                CLASSNAME (sm::SMManager & smm,                              \
+                bool poll,                                                   \
+                const IOFWDClientCB & cb,                                    \
+                int * ret,                                                   \
+                BOOST_PP_SEQ_FOR_EACH(CLIENT_GENERATEHEADERSM_PARAMS, , PARAMETERS) \
+                zoidfs::zoidfs_op_hint_t * op_hint                           \
+                ):                                                           \
+            sm::SimpleSM< iofwdclient::clientsm::CLASSNAME >(smm, poll),     \
+            slots_(*this),                                                   \
+            cb_(cb),                                                         \
+            ret_(ret),                                                       \
+            in_(INSTREAMNAME INPARAMS),                                     \
+            out_ OUTPARAMS ,                                                 \
+            server_sm_(NULL)                                                 \
+            {                                                                \
+               fprintf(stderr, "%s:%i\n", __func__, __LINE__);               \
+            }                                                                \
+               ~CLASSNAME();                                                 \
+               void init(iofwdevent::CBException e);                         \
+               void postRPCServerSM(iofwdevent::CBException e);              \
+               void waitRPCServerSM(iofwdevent::CBException e);              \
+               void postSMErrorState(iofwdevent::CBException e);             \
+          protected:                                                         \
+              enum {BASE_SLOT = 0, NUM_BASE_SLOTS};                          \
+              sm::SimpleSlots<NUM_BASE_SLOTS,                                \
+                  iofwdclient::clientsm::GetAttrClientSM> slots_;            \
+              const IOFWDClientCB & cb_;                                     \
+              int * ret_;                                                    \
+              INSTREAMNAME in_;                                              \
+              OUTSTREAMNAME out_;                                            \
+              sm::SMClientSharedPtr server_sm_;                              \
+        };                                                                   \
+    }                                                                        \
+   }                                                                         
---
+++ src/iofwdclient/streamwrappers/StreamMacro.hh
@@ -0,0 +1,86 @@
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/seq/elem.hpp>
+#include <boost/preprocessor/seq/fold_left.hpp>
+#include <boost/preprocessor/seq/cat.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/punctuation/comma.hpp>
+#include <boost/preprocessor/seq/pop_back.hpp>
+#define CLIENT_GENSTREAMWRAP_PARAMS_DECL(r,data,elem) CLIENT_GENSTREAMWRAP_DECL_S1(elem)
+#define CLIENT_GENSTREAMWRAP_DECL_S1(elem) BOOST_PP_CAT(CLIENT_GENSTREAMWRAP_S2 (elem),_);
+
+#define CLIENT_GENSTREAMWRAP_PARAMS(r,data,elem) CLIENT_GENSTREAMWRAP_S1(elem)
+#define CLIENT_GENSTREAMWRAP_S1(elem) CLIENT_GENSTREAMWRAP_S2 (elem),
+#define CLIENT_GENSTREAMWRAP_S2(elem) BOOST_PP_SEQ_FOLD_LEFT(CLIENT_DOFOLD,  \
+                                        BOOST_PP_SEQ_HEAD(elem),             \
+                                        BOOST_PP_SEQ_TAIL(elem))
+#define CLIENT_DOFOLD(r,data,elem) data elem
+#define CLIENT_GENSTREAMWRAP_PARAM_INIT(r,data,elem) CLIENT_GENSTREAMWRAP_DECPROCESS(elem)
+#define CLIENT_GENSTREAMWRAP_DECPROCESS(elem) CLIENT_GENSTREAMWRAP_PNAME(elem)(CLIENT_GENSTREAMWRAP_PICKNAME(elem)),
+#define CLIENT_GENSTREAMWRAP_PNAME(elem) BOOST_PP_CAT(BOOST_PP_SEQ_CAT(BOOST_PP_SEQ_TAIL(elem)),_)
+
+#define CLIENT_GENSTREAMWRAP_DECPARAM(r,data,elem) CLIENT_GENSTREAMWRAP_PROCESS(elem)
+#define CLIENT_GENSTREAMWRAP_PROCESS(elem) process(e,CLIENT_GENSTREAMWRAP_PROCESS2(elem));
+#define CLIENT_GENSTREAMWRAP_PICKNAME(elem) BOOST_PP_SEQ_CAT(BOOST_PP_SEQ_TAIL(elem))
+#define CLIENT_GENSTREAMWRAP_PROCESS2(elem) BOOST_PP_SEQ_FOLD_LEFT(CLIENT_WFOLD,  \
+                                        BOOST_PP_SEQ_HEAD(elem),             \
+                                        BOOST_PP_SEQ_TAIL(elem))
+#define CLIENT_WFOLD(r,data,elem) data w.elem
+#define CLIENT_GENSTREAMWRAP(CLASSNAME, INSTREAMNAME, INPARAMS, INPROCESS, OUTSTREAMNAME, OUTPARAMS, OUTPROCESS) \
+   using namespace encoder;                                                  \
+   using namespace encoder::xdr;                                             \
+   namespace iofwdclient                                                     \
+   {                                                                         \
+      namespace streamwrappers                                               \
+      {                                                                      \
+         class INSTREAMNAME                                                  \
+         {                                                                   \
+            public:                                                          \
+                 INSTREAMNAME(BOOST_PP_SEQ_FOR_EACH(CLIENT_GENSTREAMWRAP_PARAMS, , INPARAMS) \
+                              zoidfs::zoidfs_op_hint_t * op_hint) : \
+                 BOOST_PP_SEQ_FOR_EACH (CLIENT_GENSTREAMWRAP_PARAM_INIT, ,INPARAMS) \
+                 op_helper_(op_hint)                                          \
+        {                                                                     \
+        }                                                                     \
+        const encoder::OpHintHelper op_helper_;                               \
+        BOOST_PP_SEQ_FOR_EACH(CLIENT_GENSTREAMWRAP_PARAMS_DECL, ,INPARAMS)    \
+      };                                                                         \
+      class OUTSTREAMNAME                                                        \
+      {                                                                          \
+          public:                                                                \
+              GetAttrOutStream(BOOST_PP_SEQ_FOR_EACH(CLIENT_GENSTREAMWRAP_PARAMS, , OUTPARAMS) \
+                               zoidfs::zoidfs_op_hint_t * op_hint = NULL) :       \
+               BOOST_PP_SEQ_FOR_EACH(CLIENT_GENSTREAMWRAP_PARAM_INIT, ,OUTPARAMS) \
+               op_helper_(op_hint)                                                \
+           {                                                                      \
+           }                                                                      \
+           BOOST_PP_SEQ_FOR_EACH(CLIENT_GENSTREAMWRAP_PARAMS_DECL, ,OUTPARAMS)    \
+           encoder::OpHintHelper op_helper_;                                      \
+      };                                                                          \
+      template <typename Enc, typename Wrapper>                                   \
+      inline Enc & process (Enc & e,                                              \
+           Wrapper & w,                                                           \
+           typename process_filter<Wrapper, INSTREAMNAME>::type * UNUSED(d) = NULL)  \
+      {                                                                           \
+         BOOST_PP_SEQ_FOR_EACH(CLIENT_GENSTREAMWRAP_DECPARAM, ,INPROCESS)         \
+         process(e, w.op_helper_);                                                \
+         return e;                                                                \
+      }                                                                           \
+      template <typename Enc, typename Wrapper>                                   \
+      inline Enc & process (Enc & e,                                              \
+           Wrapper & w,                                                           \
+           typename process_filter<Wrapper, GetAttrOutStream>::type * UNUSED(d) = \
+           NULL)                                                                  \
+      {                                                                           \
+         BOOST_PP_SEQ_FOR_EACH(CLIENT_GENSTREAMWRAP_DECPARAM, ,OUTPROCESS)        \
+         process(e, w.op_helper_);                                                \
+         return e;                                                                \
+      }                                                                           \
+   }                                                                              \
+   }                                                                              
+
+
+
+CLIENT_GENSTREAMWRAP (GetAttrStream, GetAttrInStream, ((zoidfs_handle_t)(handle)) ,
+                      ((*)(handle)), GetAttrOutStream, ((zoidfs_attr_t)(attr)),
+                      ((*)(attr)))
+
