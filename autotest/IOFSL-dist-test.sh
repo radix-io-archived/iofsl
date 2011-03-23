bdd5ddca0e6811c12a6ae3b10169753bd50649d4
Rico D'Amore <rjdamore@lanl.gov>
2011-03-23 11:22:45 -0600

    Fixed looping problem with IOFSL-dist-test.sh
    email functionality seperated from IOFSL-dist-test.sh

--- autotest/IOFSL-dist-test.sh
+++ autotest/IOFSL-dist-test.sh
@@ -3,35 +3,6 @@
 # dependencies are located through ../scripts/configoption.${HOSTNAME}. 
 # After installation and construction the script runs a "distcheck" on the build.  
 # test reports are mailed to io-fwd-discuss@lists.mcs.anl.gov
-# This script has two options -n and -b.  -n followed by an integer identifies the number of 
-# commits you wish to test from your present position backwards through history.
-# The -b option followed by a branch name identifies which branch you would wish the test harness to exe# on.  If you wish to use the master branch you can simply leave the -b option out of your script invocation.
-
-checkout_branch(){
-  if
-    [ "$branch" != "before-resourcework-thread" ] && [ "$branch" != "iofsl_vampir" ] && [ "$branch" != "master-merge" ] && [ "$branch" != "clientrpc" ] && [ "$branch" != "paper-freeze" ] && [ "$branch" != "resourcework" ] && [ "$branch" != "tranform" ] 
-  then 
-    :
-  else
-    git checkout $branch
-  fi
-}
-
-while getopts "n:b:" opt; do
-  case $opt in
-    n)
-        num_tests="$OPTARG"
-	echo "Running tests on $OPTARG commits"
-        ;;
-    b) 
-        branch="$OPTARG"
-	checkout_branch
-	;;
-    [?])
-	echo "invalid option! only -n followed by an integer and -b followed by a branch name are valid options. See autotest/README for more info. Thanks."
-	;;
-  esac
-done
 
 get_user_email(){
   echo "Locating git user email."
@@ -61,7 +32,7 @@ read_commits(){
 execute_tests(){
   hostname=$(hostname)
   cp ~/iofsl/scripts/configoptions.tu-fe1.lanl.gov ~/iofsl/scripts/configoptions.$hostname
-  for ((i=0; i<$num_tests; i++))
+  for ((i=0; i<1; i++))
   do
     destroy_touch
   done
@@ -112,8 +83,8 @@ error_report(){
     awk 'NR<=3' runtest_results.txt >> make_error_report.txt; awk '/Error/{c=5}c&&c--' runtest_results.txt >> make_error_report.txt; tail -5 runtest_results.txt >> make_error_report.txt
     echo "=========================================================" >> make_error_report.txt
   comm=${commit:0:7}
-  echo | mutt -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@gmail.com
-  #echo | mutt -c drieskimpe@gmail.com -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@lanl.gov
+  #echo | mutt -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@gmail.com
+  echo | mutt -c committer_email rjdamore@gmail.com dkimpe@mcs.anl.gov drieskimpe@gmail.com -a make_error_report.txt -s "make error: Make FAIL for commit $comm" io-fwd-discuss@lists.mcs.anl.gov
   else test_report 
   fi
   cat make_error_report.txt >> make_error_report-pile.txt
@@ -177,8 +148,7 @@ test_report(){
   echo "test results for commit $commit" >> test_report.txt
   echo >> test_report.txt
   echo "this commit was finished testing at $(date)" >> test_report.txt
-  which gcc >> test_report.txt 
-  awk 'NR<=6' runtest_results.txt >> test_report.txt ; egrep 'Running|PASS|All' runtest_results.txt >> test_report.txt ; awk '/--Run/{c=4}c&&c--' runtest_results.txt >> test_report.txt; tail -5 runtest_results.txt >> test_report.txt
+  awk 'NR<=5' runtest_results.txt >> test_report.txt ; egrep 'Running|PASS|All' runtest_results.txt >> test_report.txt ; awk '/--Run/{c=4}c&&c--' runtest_results.txt >> test_report.txt; tail -5 runtest_results.txt >> test_report.txt
   echo "==========================================================" >> test_report.txt
   cat test_report.txt >> test_report-pile.txt
   if
@@ -188,10 +158,10 @@ test_report(){
       echo "$commit" >> autotest/tested_commits.txt
   fi
   comm=${commit:0:7}
-  echo | mutt -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
+  #echo | mutt -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
   #echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
   #echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@lanl.gov
-  #echo | mutt -c dkimpe@mcs.anl.gov -a test_report.txt -s "test report for commit $comm PASS" io-fwd-discuss@lists.mcs.anl.gov 
+  echo | mutt -c rjdamore@gmail.com drieskimpe@gmail.com dkimpe@mcs.anl.gov -a test_report.txt -s "test report for commit $comm PASS" io-fwd-discuss@lists.mcs.anl.gov 
   edit_files  
 }  
 
@@ -206,12 +176,12 @@ edit_files(){
   else
       echo "$commit" >> autotest/tested_commits.txt
   fi
-  rm -f ~/iofsl/runtest_results.txt
-  chmod -R 777 iofwd-0.1
-  rm -rf iofwd-0.1
-  rm -rf iofwd-0.1.tar.gz
-  make clean
-  make distclean
+  #rm -f ~/iofsl/runtest_results.txt
+  #chmod -R 777 iofwd-0.1
+  #rm -rf iofwd-0.1
+  #rm -rf iofwd-0.1.tar.gz
+  #make clean
+  #make distclean
   git checkout master
 }
 
--- autotest/email_results.sh
+++ autotest/email_results.sh
@@ -1,80 +1,11 @@
 #!/bin/bash
-# This script installs the IOFSL software, and executes distcheck on the IOFSL software.  
-# dependencies are located through ../scripts/configoption.${HOSTNAME}. 
-# After installation and construction the script runs a "distcheck" on the build.  
-# test reports are mailed to io-fwd-discuss@lists.mcs.anl.gov
+# This script gathers relevant information from the IOFSL-dist-test.sh script test results and mails them to the desired parties.  The mutt mail commands can be edited for the desired paties to be mailed to.  
 
-get_user_email(){
-  echo "Locating git user email."
-  export user_email=$(git config -l | grep user.email=* | sed 's/user.email=//g')  
-  if [[ $user_email == *@* ]]
-  then
-    echo "User email found ----> "   $user_email
-  else
-    echo "Have you configured your git global email? It's needed for error reporting"
-  fi
-  commits_to_txt
-}
-  
-commits_to_txt(){
-  git log | grep "commit" | awk '{print $2}'| awk 'length > 39'  > commits_to.txt
-  read_commits
-}
-
-read_commits(){
-  commits_IFS=$IFS
-  IFS=$'\n'
-  lines=($(cat commits_to.txt))
-  IFS=$commits_IFS 
-  execute_tests
-}
-
-execute_tests(){
-  hostname=$(hostname)
-  cp ~/iofsl/scripts/configoptions.tu-fe1.lanl.gov ~/iofsl/scripts/configoptions.$hostname
-  for ((i=0; i<1; i++))
-  do
-    destroy_touch
-  done
-}
-
-destroy_touch(){
-  #make clean
-  #make distclean
-  touch test_report.txt
-  touch failure_report.txt
-  touch make_error_report.txt  
-  touch runtest_results.txt
-  set_to_variable
-}
-
-set_to_variable(){
-  export commit=$(echo ${lines[$i]})
-  echo "testing commit $commit"
-  if
-    grep $commit autotest/tested_commits.txt
-  then
-    echo "This commit has been tested previously...exiting testing schedule..." ; exit 0
-  fi
-  export committer_email=$(git cat-file commit $commit | grep committer | awk '{print $4}' | sed 's/<//g' | sed 's/>//g')
-  echo "committer_email= $committer_email"
-  echo $(date) >> runtest_results.txt
-  echo "testing commit $commit" >> runtest_results.txt
-  echo "commit $commit committed by $committer_email" >> runtest_results.txt
-  run_tests
-}
-
-run_tests(){
-  git checkout $commit
-  scripts/runtest.sh 2>&1 | tee -a runtest_results.txt 
-  echo "This concludes the test of commit $commit" >> runtest_results.txt
-  error_report
-}
 
 error_report(){
   rm -f make_error_report.txt
   touch make_error_report.txt
-  if 
+  if
     egrep ' Error ' runtest_results.txt
   then
     echo "test results for commit $commit" >> make_error_report.txt
@@ -83,8 +14,9 @@ error_report(){
     awk 'NR<=3' runtest_results.txt >> make_error_report.txt; awk '/Error/{c=5}c&&c--' runtest_results.txt >> make_error_report.txt; tail -5 runtest_results.txt >> make_error_report.txt
     echo "=========================================================" >> make_error_report.txt
   comm=${commit:0:7}
-  echo | mutt -c drieskimpe@gmail.com -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@lanl.gov
-  else test_report 
+  echo | mutt -a make_error_report.txt -s "make error: Make FAIL for commit $comm" rjdamore@gmail.com
+  #echo | mutt -c  $committer_email dkimpe@mcs.anl.gov drieskimpe@gmail.com rjdamore@gmail.com -a make_error_report.txt -s "make error: Make FAIL for commit $comm" io-fwd-discuss@lists.mcs.anl.gov
+  else test_report
   fi
   cat make_error_report.txt >> make_error_report-pile.txt
   if
@@ -95,6 +27,7 @@ error_report(){
   fi
   failure_report
 }
+
 
 failure_report(){
   rm -f failure_report.txt
@@ -107,8 +40,8 @@ failure_report(){
     echo >> failure_report.txt
     awk 'NR<=5' runtest_results.txt >> failure_report.txt ; awk '/FAIL/{c=5}c&&c--' runtest_results.txt >> failure_report.txt ; awk '/non-zero/{c=3}c&&c--' runtest_results.txt >> failure_report.txt; tail -5 runtest_results.txt >> failure_report.txt
     echo "==========================================================" >> failure_report.txt
-  comm=${commit:0:7}   
-  echo | mutt -a failure_report.txt -s "failure report for commit $comm FAIL" rjdamore@gmail.com
+  comm=${commit:0:7}
+  i#echo | mutt -c $committer_email dkimpe@mcs.anl.gov drieskimpe@gmail.com rjdamore@gmail.com -a failure_report.txt -s "failure report for commit $comm FAIL" io-fwd-discuss@lists.mcs.anl.gov 
   else :
   fi
   cat failure_report.txt >> failure_report-pile.txt
@@ -120,7 +53,7 @@ failure_report(){
   fi
   server_report
 }
-    
+
 server_report(){
   rm -f server_report.txt
   touch server_report.txt
@@ -138,7 +71,7 @@ server_report(){
   fi
   comm=${commit:0:7}
   echo | mutt -a server_report.txt -s "server report for commit $comm" rjdamore@gmail.com
-  test_report
+
 }
 
 test_report(){
@@ -147,7 +80,8 @@ test_report(){
   echo "test results for commit $commit" >> test_report.txt
   echo >> test_report.txt
   echo "this commit was finished testing at $(date)" >> test_report.txt
-  awk 'NR<=5' runtest_results.txt >> test_report.txt ; egrep 'Running|PASS|All' runtest_results.txt >> test_report.txt ; awk '/--Run/{c=4}c&&c--' runtest_results.txt >> test_report.txt; tail -5 runtest_results.txt >> test_report.txt
+  which gcc >> test_report.txt
+  awk 'NR<=6' runtest_results.txt >> test_report.txt ; egrep 'Running|PASS|All' runtest_results.txt >> test_report.txt ; awk '/--Run/{c=4}c&&c--' runtest_results.txt >> test_report.txt; tail -5 runtest_results.txt >> test_report.txt
   echo "==========================================================" >> test_report.txt
   cat test_report.txt >> test_report-pile.txt
   if
@@ -157,31 +91,30 @@ test_report(){
       echo "$commit" >> autotest/tested_commits.txt
   fi
   comm=${commit:0:7}
-  echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
+  echo | mutt -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
+  #echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@gmail.com
   #echo | mutt -c drieskimpe@gmail.com -a test_report.txt -s "test report for commit $comm PASS" rjdamore@lanl.gov
-  #echo | mutt -c dkimpe@mcs.anl.gov -a test_report.txt -s "test report for commit $comm PASS" io-fwd-discuss@lists.mcs.anl.gov 
-  edit_files  
-}  
+  #echo | mutt -c dkimpe@mcs.anl.gov drieskimpe@gamail.com rjdamore@gmail.com -a test_report.txt -s "test report for commit $comm PASS" io-fwd-discuss@lists.mcs.anl.gov 
+  edit_files
+}
 
 edit_files(){
-  echo "==========================================================" >> ~/repo-testing-results-pile.txt
-  echo $(date) >> ~/repo-testing-results-pile.txt
-  cat ~/iofsl/runtest_results.txt >>  ~/repo-testing-results-pile.txt
-  echo "==========================================================" >> ~/repo-testing-results-pile.txt
+  echo "==========================================================" >> repo-testing-results-pile.txt
+  echo $(date) >> repo-testing-results-pile.txt
+  cat runtest_results.txt >>  repo-testing-results-pile.txt
+  echo "==========================================================" >> repo-testing-results-pile.txt
   if
     grep $commit autotest/tested_commits.txt
   then :
   else
       echo "$commit" >> autotest/tested_commits.txt
   fi
-  rm -f ~/iofsl/runtest_results.txt
-  #chmod -R 777 iofwd-0.1
-  #rm -rf iofwd-0.1
-  #rm -rf iofwd-0.1.tar.gz
+  #rm -f runtest_results.txt
+  chmod -R 777 iofwd-1.0.0
+  rm -rf iofwd-1.0.0
+  rm -rf iofwd-1.0.0.tar.gz
   make clean
   make distclean
   git checkout master
 }
-
-
-get_user_email
+error_report
