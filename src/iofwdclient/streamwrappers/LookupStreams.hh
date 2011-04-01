d2cb471fc0b0a459a20693744417a19ab3755a68
Benjamin Welton <welton@mcs.anl.gov>
2011-04-01 02:11:09 -0500

    Working Lookup client -> server communication (server encoding still an issue)

--- src/iofwdclient/streamwrappers/LookupStreams.hh
+++ src/iofwdclient/streamwrappers/LookupStreams.hh
@@ -6,7 +6,9 @@
 #include "zoidfs/util/OpHintHelper.hh"
 #include "iofwdutil/tools.hh"
 #include "encoder/Util.hh"
+#include "encoder/EncoderWrappers.hh"
 #include "zoidfs/util/zoidfs-wrapped.hh"
+#include "zoidfs/util/FileSpecHelper.hh"
 #include <cstdio>
 using namespace encoder;
 using namespace encoder::xdr;
@@ -62,12 +64,14 @@ inline Enc & process (Enc & e,
         typename process_filter<Wrapper, LookupInStream>::type * UNUSED(d) = NULL,
          typename only_size_processor<Enc>::type * = 0)
 {
-   zoidfs::zoidfs_null_param_t haveFullPath;
-   haveFullPath = 0;
-   process (e, haveFullPath);
-   process(e,*(w.parent_handle_));
-   process(e, EncOpaque(NULL, ZOIDFS_NAME_MAX, ZOIDFS_NAME_MAX));
-   process(e, EncOpaque(NULL, ZOIDFS_PATH_MAX, ZOIDFS_PATH_MAX));
+   FileSpecHelper x(w.parent_handle_, w.component_name_, w.full_path_);
+   process (e,x);
+//   zoidfs::zoidfs_null_param_t haveFullPath;
+//   haveFullPath = 0;
+//   process (e, haveFullPath);
+//   process(e,*(w.parent_handle_));
+//   process(e, EncOpaque(NULL, ZOIDFS_NAME_MAX, ZOIDFS_NAME_MAX));
+//   process (e, EncString(w.full_path_, ZOIDFS_PATH_MAX));
 //   process(e,w.op_helper_);
 }
 
@@ -77,22 +81,25 @@ inline Enc & process (Enc & e,
         typename process_filter<Wrapper, LookupInStream>::type * UNUSED(d) = NULL,
         typename only_encoder_processor<Enc>::type * = NULL)
 {
-    fprintf(stderr, "LOOKUPSTERAMS:%s:%i\n", __func__, __LINE__);
-    fprintf(stderr, "LOOKUPSTERAMS:%s:%s\n",w.full_path_, w.component_name_);
-    zoidfs::zoidfs_null_param_t haveFullPath;
-    haveFullPath = (w.full_path_ != NULL) ? 1 : 0;
-    process (e, haveFullPath);
-    if (haveFullPath)
-    {
-       process(e, EncOpaque (w.full_path_, strlen(w.full_path_), ZOIDFS_PATH_MAX));
-    }
-    else
-    {
-       process(e, *(w.parent_handle_));
+   FileSpecHelper x(w.parent_handle_, w.component_name_, w.full_path_);
+      process (e,x);
+//    fprintf(stderr, "LOOKUPSTERAMS:%s:%i\n", __func__, __LINE__);
+//    fprintf(stderr, "LOOKUPSTERAMS:%s:%s\n",w.full_path_, w.component_name_);
+//    zoidfs::zoidfs_null_param_t haveFullPath;
+//    haveFullPath = (w.full_path_ != NULL) ? 1 : 0;
+//    process (e, haveFullPath);
+//    if (haveFullPath)
+//    {
+//       process (e, EncString(w.full_path_, ZOIDFS_PATH_MAX));
+//       //process(e, EncOpaque (w.full_path_, strlen(w.full_path_), ZOIDFS_PATH_MAX));
+//    }
+//    else
+//    {
+//       process(e, *(w.parent_handle_));
 
-       process(e, EncOpaque (w.component_name_, strlen(w.component_name_), ZOIDFS_NAME_MAX));
-    }
-//    process(e, w.op_helper_);
+//       process(e, EncOpaque (w.component_name_, strlen(w.component_name_), ZOIDFS_NAME_MAX));
+//    }
+////    process(e, w.op_helper_);
 
     return e;
 }
