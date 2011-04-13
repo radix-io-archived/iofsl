4579b4a6870656fa100400c5fa06c0ae57d2f57c
Benjamin Welton <welton@mcs.anl.gov>
2011-04-13 14:24:40 -0500

    Added python based testing scripts for fusion benchmark
    Signed-off-by: Benjamin Welton <welton@mcs.anl.gov>

--- test/benchmark/fusion/RunBenchmark.py
+++ test/benchmark/fusion/RunBenchmark.py
@@ -88,6 +88,9 @@ for x in nodes:
 
 f.close()
 
+# Copy testfiles to test directory 
+shutil.copytree (test_files, os.path.join(tmp_dir,"test_files"))
+
 for root, dirs, files in os.walk(os.path.join(tmp_dir,"test_files")):
   for name in files:
     file_name =  os.path.join ( root, name )
@@ -95,25 +98,24 @@ for root, dirs, files in os.walk(os.path.join(tmp_dir,"test_files")):
 
     # Execute Server 
     os.putenv("ZOIDFS_SERVER_RANK","0")
-    server = Popen([server_exe,"--config",os.path.join(config_dir,"serverconf.conf")])
+    server = Popen([server_exe,"--config",os.path.join(tmp_dir,"serverconf.conf")])
     logging.info("\tServer Started")
-    logging.info("\t\t" + server_exe + " --config " + os.path.join(config_dir,"serverconf.conf"))
+    logging.info("\t\t" + server_exe + " --config " + os.path.join(tmp_dir,"serverconf.conf"))
   
 
     #Execute Client 
     nodefile = os.path.join(tmp_dir, "nodelist")
-    #"-f", nodefile,
-    client = Popen(["mpiexec", "-n", str(cores), 
+    client = Popen(["mpiexec", "-n", str(cores), "-f", nodefile,
                     client_exe, "tcp://" + ServerNode + ":9001", 
-                    os.path.join(config_dir,"clientconf.conf"), file_name, 
+                    os.path.join(tmp_dir,"clientconf.conf"), file_name, 
                     "/dev/null",  str(os.path.getsize(file_name)), result_file])
     logging.info ("\tClient Started")
     logging.info ("\t\t" + "mpiexec" + " -n " + str(cores) + " -f \n\t\t\t" + nodefile +
                   " " + client_exe + " tcp://" + ServerNode + ":9001\n\t\t\t" +
-                  os.path.join(config_dir,"clientconf.conf") + " " + file_name +  
+                  os.path.join(tmp_dir,"clientconf.conf") + " " + file_name +  
                   "\n\t\t\t/dev/null " +  str(os.path.getsize(file_name)) + " " + result_file)
 
     client.wait()
-    server.kill()
+    server.terminate()
 
 
