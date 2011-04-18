59ee37143b0892221743b034539eab2ffdde7eca
Benjamin Welton <welton@mcs.anl.gov>
2011-04-18 16:03:50 -0500

    Read/Write task 32 bit compatability fixes

--- src/iofwd/tasksm/WriteTaskSM.cpp
+++ src/iofwd/tasksm/WriteTaskSM.cpp
@@ -64,9 +64,10 @@ namespace iofwd
               p.file_count, p.file_starts.get(), p.file_sizes.get(),
               const_cast<zoidfs::zoidfs_op_hint_t *>(p.op_hint));
 #else
-        api_->write(slots_[WRITE_SLOT], &ret_, p.handle, p.mem_count, (const
-                 void**)p.mem_starts, p.mem_sizes, p.file_count, p.file_starts,
-              p.file_sizes, p.op_hint);
+        api_->write(slots_[WRITE_SLOT], &ret_, p.handle, p.mem_count,
+              const_cast<const void**>(reinterpret_cast<void**>(p.mem_starts.get())), p.mem_sizes.get(),
+              p.file_count, p.file_starts.get(), p.file_sizes.get(),
+              const_cast<zoidfs::zoidfs_op_hint_t *>(p.op_hint));
 #endif
 
         /* set the callback */
