676f46186fab5130bce2c372b6f047e5d34eeecf
Benjamin Welton <welton@mcs.anl.gov>
2011-05-19 14:39:09 -0500

    Updated benchmark tools

--- test/benchmark/BenchmarkMain.cpp
+++ test/benchmark/BenchmarkMain.cpp
@@ -1,7 +1,15 @@
 #include "BenchmarkMain.hh"
 #include <iostream>
 #include <fstream>
+#include <cstdio>
+using namespace iofwd;
+using namespace iofwdevent;
+using namespace boost;
+using namespace iofwd::service;
+using namespace iofwd::extraservice;
 using namespace std;
+using namespace iofwdevent;
+using namespace rpc;
 
 void LogResult ( ofstream * f, char * s, double runTime, size_t outLen,
                 char * filename)
@@ -9,12 +17,13 @@ void LogResult ( ofstream * f, char * s, double runTime, size_t outLen,
   char * outStr = new char[strlen(s) + 45 + strlen(filename)];
   sprintf(outStr,"%s,%s,%10.8g,%i\n", s, filename, runTime, outLen);
   f->write(outStr, strlen(outStr));
-
+  f->flush();
+  fprintf(stderr,"%s,%s,%10.8g,%i\n", s, filename, runTime, outLen);
   delete[] outStr;
 }
 
 void control (char * UNUSED(address), char * UNUSED(config), char * inDataset, 
-              char * UNUSED(outDataset), int UNUSED(readSize), int  runs, char* csvOutput)
+              char * outDataset, int readSize, int  runs, char* csvOutput)
 {
   ofstream logfile;
   double start, end;
@@ -30,31 +39,64 @@ void control (char * UNUSED(address), char * UNUSED(config), char * inDataset,
      start = MPI_Wtime();
      MPI_Barrier(MPI_COMM_WORLD);
      end = MPI_Wtime();
-     LogResult(&logfile, "READ", end - start, 0, inDataset);
+     LogResult(&logfile, "READ", end - start, readSize, inDataset);
      MPI_Barrier(MPI_COMM_WORLD); 
      start = MPI_Wtime();
      MPI_Barrier(MPI_COMM_WORLD);
      end = MPI_Wtime();
-     LogResult(&logfile, "WRITE", end - start, 0, inDataset);
+     LogResult(&logfile, "WRITE", end - start, readSize, outDataset);
   }
 }
+/*
+void doLookup (iofwdclient::IOFWDClient * x, zoidfs::zoidfs_handle_t * outHandle, char * filename)
+{
+   zoidfs::zoidfs_op_hint_t op_hint;
+   zoidfs::hints::zoidfs_hint_create(&op_hint);
+   x->lookup (NULL, NULL, filename, outHandle, &op_hint);
+}
 
+void getHandles ( char * address, std::string opt_config, zoidfs::zoidfs_handle_t * outHandle, char * outfilename,
+                   zoidfs::zoidfs_handle_t * inHandle, char * infilename)
+{
+   // Needed to autoregister services
+   registerIofwdFactoryClients ();
+   ServiceManager & man = ServiceManager::instance ();
+   man.setParam ("config.configfile", opt_config);
+   shared_ptr<iofwd::Net> netservice (man.loadService<iofwd::Net>("net"));
+   SingleCompletion block;
+   net::Net * net = netservice->getNet ();
+   net::AddressPtr addr;
+   net->lookup (address, &addr, block);
+   block.wait ();
+   iofwdclient::IOFWDClient * x  =  new iofwdclient::IOFWDClient (*(new iofwdclient::CommStream()),
+                                                                  addr,
+                                                                  (bool)true);
+   doLookup(x, outHandle, outfilename);
+   doLookup(x, inHandle, infilename);
+
+   delete x;
+}
+*/
 int main(int argc, char *argv[]) 
 {
-    int runs = 21;
+    int runs = 6;
     if (argc < 4)
       return -1;
 
-    int rank, size; 
-    MPI::Init(argc, argv);           
-    size = MPI::COMM_WORLD.Get_size(); 
-    rank = MPI::COMM_WORLD.Get_rank(); 
+    int n, rank, size, i; 
     char * address = argv[1];
     char * config = argv[2];
     char * inDataset = argv[3];
     char * outDataset = argv[4];
     int readSize = std::atoi(argv[5]);
     char * csvOutput = argv[6];    
+    //zoidfs::zoidfs_handle_t outHandle;
+    //zoidfs::zoidfs_handle_t inHandle;
+    //getHandles (address, config, &outHandle, outDataset, &inHandle, inDataset);
+
+    MPI::Init(argc, argv);           
+    size = MPI::COMM_WORLD.Get_size(); 
+    rank = MPI::COMM_WORLD.Get_rank(); 
     if (rank == 0)
       control (address, config, inDataset, outDataset, readSize, runs, csvOutput);
     else
--- test/benchmark/fusion/RunBlockBenchmark_eth.py
+++ test/benchmark/fusion/RunBlockBenchmark_eth.py
@@ -32,11 +32,18 @@ compression = config.get ("Benchmark", "Compression")
 tmp_dir = config.get ("Benchmark", "TempDirectory")
 result_file = config.get ("Benchmark", "ResultFile")
 config_dir = config.get ("Benchmark", "ConfigurationDirectory")
+block_size = config.get ("Benchmark", "BlockSizes")
+output_file = config.get ("Benchmark", "OutputFile")
+input_file = config.get ("Benchmark", "InputFile")
+
 logging.info ("Configuration Settings")
 logging.info ("\tConfiguration File: " + str(config_file))
 logging.info ("\tNumber of Cores: " + str(cores))
 logging.info ("\tServer Exe: " + str(server_exe))
 logging.info ("\tClient Exe: " + str(client_exe))
+logging.info ("\tOutput File: " + str(output_file))
+logging.info ("\tInput File: " + str(input_file))
+logging.info ("\tBlock Sizes: " + str(block_size))
 logging.info ("\tTest File Directory: " + str(test_files))
 logging.info ("\tOutput Result File: " + str(result_file))
 logging.info ("\tCompression: " + str(compression))
@@ -58,7 +65,12 @@ for x in nodes:
   logging.info ("\tNode " + x + ": " + str(nodes[x]))
 
 # Copy testfiles to test directory 
-# shutil.copytree (test_files, os.path.join(tmp_dir,"test_files"))
+#shutil.copytree (test_files, os.path.join(tmp_dir,"test_files"))
+#if !os.path.exists(os.path.join(tmp_dir,"test_files")):
+try:
+    os.makedirs(os.path.join(tmp_dir,"test_files"))
+except:
+    pass 
 
 # Generate Server Configuration
 hostname = os.uname()[1]
@@ -66,7 +78,7 @@ ServerNode = hostname
 ServerProcCount = nodes[ServerNode]
 del nodes[ServerNode]
 
-server_conf = server_template % str("tcp://" + ServerNode + "-ib:9001")
+server_conf = server_template % str("tcp://" + ServerNode + ":9001")
 f = open ( os.path.join(config_dir,"serverconf.conf"), "w")
 f.write(server_conf)
 f.close()
@@ -94,11 +106,13 @@ f.close()
 #server = Popen([server_exe,"--config",os.path.join(config_dir,"serverconf.conf")])
 #logging.info("\tServer Started")
 #logging.info("\t\t" + server_exe + " --config " + os.path.join(config_dir,"serverconf.conf"))
-  
-for root, dirs, files in os.walk(os.path.join(tmp_dir,"test_files")):
-  for name in files:
-    file_name =  os.path.join ( root, name )
-    logging.info ("File Test: " + file_name)
+
+tmp = block_size.split(",")
+for bsize in tmp:
+#for root, dirs, files in os.walk(os.path.join(tmp_dir,"test_files")):
+#  for name in files:
+#    file_name =  os.path.join ( root, name )
+#    logging.info ("File Test: " + file_name)
 
     # Execute Server 
     os.putenv("ZOIDFS_SERVER_RANK","0")
@@ -108,16 +122,29 @@ for root, dirs, files in os.walk(os.path.join(tmp_dir,"test_files")):
     time.sleep(20)
     #Execute Client 
     nodefile = os.path.join(tmp_dir, "nodelist")
+
+    if compression.lower() == "zlib":
+      os.putenv("IOFSL_RPC=transform:ZLIB", "0")
+      logging.info ("\tCompression Flag Set ZLIB")
+    elif compression.lower() == "bzlib":
+      os.putenv("IOFSL_RPC=transform:BZLIB", "0")
+      logging.info ("\tCompression Flag Set BZLIB")
+    elif compression.lower() == "lzf":
+      os.putenv("IOFSL_RPC=transform:LZF", "0")
+      logging.info ("\tCompression Flag Set LZF")
+    elif compression.lower() == "lzo":
+      os.putenv("IOFSL_RPC=transform:LZO", "0")
+      logging.info ("\tCompression Flag Set LZO")
     #"-f", nodefile,
     client = Popen(" ".join(["/soft/mpich2/1.3-gcc-4.1.2/bin/mpiexec", "-n", str(cores), "-f", nodefile,
-                    client_exe, "tcp://" + ServerNode + "-ib:9001", 
-                    os.path.join(config_dir,"clientconf.conf"), file_name, 
-                    "/dev/null",  str(os.path.getsize(file_name)), result_file]), shell=True)
+                    client_exe, "tcp://" + ServerNode + ":9001", 
+                    os.path.join(config_dir,"clientconf.conf"), str(input_file), 
+                    str(output_file), str(bsize), result_file + "_" + bsize]), shell=True)
     logging.info ("\tClient Started")
     logging.info ("\t\t" + "mpiexec" + " -n " + str(cores) + " -f \n\t\t\t" + nodefile +
                   " " + client_exe + " tcp://" + ServerNode + "-ib:9001\n\t\t\t" +
-                  os.path.join(config_dir,"clientconf.conf") + " " + file_name +  
-                  "\n\t\t\t/dev/null " +  str(os.path.getsize(file_name)) + " " + result_file)
+                  os.path.join(config_dir,"clientconf.conf") + " " + str(input_file) +  
+                  "\n\t\t\t" + str(output_file) + " " + str(bsize) + " " + result_file + "_" + bsize )
 
     client.wait()
     server.kill()
