53bb1f69ac4a6c4a97ef405728b20bba1905fb45
Benjamin Welton <welton@mcs.anl.gov>
2011-05-19 18:15:29 -0500

    Added GetAttr server request.
    references #86

--- src/iofwd/rpcfrontend/IOFSLRPCGetAttrRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCGetAttrRequest.cpp
@@ -6,49 +6,30 @@ namespace iofwd
    namespace rpcfrontend
    {
 
-      RPC_GENPROCESS (IOFSLRPCGetAttrRequest, ((&)(handle))
-                                              ((&)(attr)),
-                                              (()(returnCode))
-                                              ((*)(attr_enc_)))
-
-
       const IOFSLRPCGetAttrRequest::ReqParam & IOFSLRPCGetAttrRequest::decodeParam() 
       { 
-          decodeRPCInput(); 
-          param_.handle = &dec_struct.handle ; 
-          param_.attr = &dec_struct.attr ; 
+          param_.handle = &inStruct.handle ; 
+          param_.attr = &inStruct.attr ; 
           param_.op_hint = &op_hint_; 
           return param_; 
       }
-      void IOFSLRPCGetAttrRequest::reply(const CBType & UNUSED(cb),
-              const zoidfs::zoidfs_attr_t * attr)
+      void IOFSLRPCGetAttrRequest::reply(const CBType & cb,
+                                         const zoidfs::zoidfs_attr_t * attr)
       {
           /* verify the args are OK */
           ASSERT(getReturnCode() != zoidfs::ZFS_OK || attr);
 
           /* store ptr to the attr */
-          attr_enc_ = const_cast<zoidfs::zoidfs_attr_t *>(attr);
+          outStruct.attr_enc = (*attr);
 
           /* encode */
-          encodeRPCOutput();
+          encode(cb);
 
-          /* invoke the callback */
-          //cb();
       }
 
       IOFSLRPCGetAttrRequest::~IOFSLRPCGetAttrRequest()
       {
          zoidfs::hints::zoidfs_hint_free(&op_hint_);
-      }
-
-      size_t IOFSLRPCGetAttrRequest::rpcEncodedInputDataSize()
-      {
-          return 0;
-      }
-
-      size_t IOFSLRPCGetAttrRequest::rpcEncodedOutputDataSize()
-      {
-          return 0;
       }
 
    }
