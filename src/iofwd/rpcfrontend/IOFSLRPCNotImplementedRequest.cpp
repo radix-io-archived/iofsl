259abe35bc6adf157466edbfe09152c1dd66a5c8
Benjamin Welton <welton@mcs.anl.gov>
2011-05-20 12:30:54 -0500

    Added not implimented request (server)
    references #86

--- src/iofwd/rpcfrontend/IOFSLRPCNotImplementedRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCNotImplementedRequest.cpp
@@ -4,13 +4,13 @@ namespace iofwd
 {
    namespace rpcfrontend
    {
-        void IOFSLRPCNotImplementedRequest::reply (const CBType & cb)
-        {
-          /* verify the args are OK */
-          ASSERT(getReturnCode() != zoidfs::ZFS_OK);
-          /* encode */
-          encodeRPCOutput();
-        }
+      void IOFSLRPCNotImplementedRequest::reply (const CBType & cb)
+      {
+        /* verify the args are OK */
+        ASSERT(getReturnCode() != zoidfs::ZFS_OK);
+        outStruct.returnCode = getReturnCode();
+        encode(cb);
+      }
       IOFSLRPCNotImplementedRequest::~IOFSLRPCNotImplementedRequest()
       {
       }
--- src/iofwd/rpcfrontend/IOFSLRPCNotImplementedRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCNotImplementedRequest.hh
@@ -5,41 +5,37 @@
 
 #include "zoidfs/util/zoidfs-wrapped.hh"
 #include "zoidfs/zoidfs.h"
-
+#include "encoder/EncoderStruct.hh"
+#include "encoder/EncoderString.hh"
 #include "iofwd/NotImplementedRequest.hh"
-#include "iofwd/rpcfrontend/IOFSLRPCRequest.hh"
+#include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
 #include "iofwdevent/CBType.hh"
+
 namespace iofwd
 {
    namespace rpcfrontend
    {
+      ENCODERSTRUCT (RPCNotImpliin, );
+      ENCODERSTRUCT (RPCNotImpliOut, ((int)(returnCode)))
       class IOFSLRPCNotImplementedRequest :
-          public IOFSLRPCRequest,
+          public RPCSimpleRequest<RPCNotImpliin, RPCNotImpliOut>,
           public NotImplementedRequest
       {
           public:
               IOFSLRPCNotImplementedRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  IOFSLRPCRequest(in, out),
+                  RPCSimpleRequest<RPCNotImpliin, RPCNotImpliOut>(in, out),
                   NotImplementedRequest(opid)
               {
               }
             
               ~IOFSLRPCNotImplementedRequest();
-
-              /* encode and decode helpers for RPC data */
-              void decode() {};
-              void encode() { process(enc_, getReturnCode()); };
-
               /* request processing */
               const ReqParam & decodeParam() {return param_;};
               void reply (const CBType & cb);
           protected:
               /* data size helpers for this request */ 
-              virtual size_t rpcEncodedInputDataSize()  { return 0; }; 
-              virtual size_t rpcEncodedOutputDataSize() { return 0; };
-
               ReqParam param_;
       };
    }
