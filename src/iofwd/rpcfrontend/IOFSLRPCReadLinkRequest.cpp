bcf5ebf4f146da854c335b6fa14e518ed7b570c8
Benjamin Welton <welton@mcs.anl.gov>
2011-05-20 11:31:57 -0500

    Server Read link request
    references #86

--- src/iofwd/rpcfrontend/IOFSLRPCReadLinkRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCReadLinkRequest.cpp
@@ -6,18 +6,10 @@ namespace iofwd
 {
    namespace rpcfrontend
    {
-      RPC_GENPROCESS (IOFSLRPCReadLinkRequest,((&)(handle))
-                                              (()(buffer_length)) ,
-                                              (()(returnCode))
-                                              (()(buffer))
-                                              (()(buffer_length)))
-                                            
-
       const IOFSLRPCReadLinkRequest::ReqParam & IOFSLRPCReadLinkRequest::decodeParam() 
       { 
-          decodeRPCInput(); 
-          param_.handle = &dec_struct.handle; 
-          param_.buffer_length = dec_struct.buffer_length;
+          param_.handle = &inStruct.handle; 
+          param_.buffer_length = inStruct.buffer_length;
           param_.op_hint = &op_hint_; 
           return param_; 
       }
@@ -27,30 +19,15 @@ namespace iofwd
       {
           /* verify the args are OK */
           ASSERT(getReturnCode() != zoidfs::ZFS_OK || buffer_ || buffer_length_);
-
-          buffer.value.assign(buffer_);
-          buffer_length = buffer_length_;
-
+          outStruct.buffer.value.assign(buffer_);
+          outStruct.buffer_length = buffer_length_;
           /* encode */
-          encodeRPCOutput();
-
-          /* invoke the callback */
-          //cb();
+          encode(cb);
       }
 
       IOFSLRPCReadLinkRequest::~IOFSLRPCReadLinkRequest()
       {
          zoidfs::hints::zoidfs_hint_free(&op_hint_);
-      }
-
-      size_t IOFSLRPCReadLinkRequest::rpcEncodedInputDataSize()
-      {
-          return 0;
-      }
-
-      size_t IOFSLRPCReadLinkRequest::rpcEncodedOutputDataSize()
-      {
-          return 0;
       }
 
    }
--- src/iofwd/rpcfrontend/IOFSLRPCReadLinkRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCReadLinkRequest.hh
@@ -5,15 +5,13 @@
 #include "zoidfs/zoidfs.h"
 
 #include "iofwd/ReadLinkRequest.hh"
-#include "iofwd/rpcfrontend/IOFSLRPCRequest.hh"
-
+#include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
 namespace iofwd
 {
    namespace rpcfrontend
    {
       typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
       typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
-
       typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
       ENCODERSTRUCT (IOFSLRPCReadLinkDec, ((zoidfs_handle_t)(handle))
                                           ((size_t)(buffer_length)))
@@ -23,23 +21,19 @@ namespace iofwd
                                           ((size_t)(buffer_length)))
 
       class IOFSLRPCReadLinkRequest :
-          public IOFSLRPCRequest,
+          public RPCSimpleRequest<IOFSLRPCReadLinkDec, IOFSLRPCReadLinkEnc>,
           public ReadLinkRequest
       {
           public:
               IOFSLRPCReadLinkRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  IOFSLRPCRequest(in, out),
+                  RPCSimpleRequest<IOFSLRPCReadLinkDec, IOFSLRPCReadLinkEnc>(in, out),
                   ReadLinkRequest(opid)
               {
               }
             
               virtual ~IOFSLRPCReadLinkRequest();
-
-              /* encode and decode helpers for RPC data */
-              virtual void decode();
-              virtual void encode();
 
               /* request processing */
               virtual const ReqParam & decodeParam();
@@ -48,14 +42,7 @@ namespace iofwd
                       
           protected:
               /* data size helpers for this request */ 
-              virtual size_t rpcEncodedInputDataSize(); 
-              virtual size_t rpcEncodedOutputDataSize();
-
               ReqParam param_;
-              EncoderString buffer;
-              size_t buffer_length;
-              IOFSLRPCReadLinkDec dec_struct;
-              IOFSLRPCReadLinkEnc enc_struct;
               zoidfs::zoidfs_op_hint_t op_hint_;
       };
 
