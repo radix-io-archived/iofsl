c186d341b98e307c96a3cf2de8d725079971da34
Benjamin Welton <welton@mcs.anl.gov>
2011-05-20 12:48:02 -0500

    Added set attr request (server).
    references #86

--- src/iofwd/rpcfrontend/IOFSLRPCSetAttrRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCSetAttrRequest.cpp
@@ -5,54 +5,30 @@ namespace iofwd
 {
    namespace rpcfrontend
    {
-
-      RPC_GENPROCESS (IOFSLRPCSetAttrRequest, ((&)(handle))
-                                              ((&)(attr))
-                                              ((&)(sattr)),
-                                              (()(returnCode))
-                                              ((*)(attr)))
-
-
       const IOFSLRPCSetAttrRequest::ReqParam & IOFSLRPCSetAttrRequest::decodeParam() 
       { 
-          decodeRPCInput(); 
-          param_.handle = &dec_struct.handle ; 
-          param_.attr = &dec_struct.attr; 
-          param_.sattr = &dec_struct.sattr;
+          param_.handle = &inStruct.handle ; 
+          param_.attr = &inStruct.attr; 
+          param_.sattr = &inStruct.sattr;
 
           param_.op_hint = &op_hint_; 
           return param_; 
       }
-      void IOFSLRPCSetAttrRequest::reply(const CBType & UNUSED(cb),
+      void IOFSLRPCSetAttrRequest::reply(const CBType & cb,
               const zoidfs::zoidfs_attr_t * attr)
       {
           /* verify the args are OK */
-          ASSERT(getReturnCode() != zoidfs::ZFS_OK || attr);
+        ASSERT(getReturnCode() != zoidfs::ZFS_OK || attr);
 
-          /* store ptr to the attr */
-          attr = const_cast<zoidfs::zoidfs_attr_t *>(attr);
-
-          /* encode */
-          encodeRPCOutput();
-
-          /* invoke the callback */
-          //cb();
+        /* store ptr to the attr */
+        outStruct.attr = *attr;
+        outStruct.returnCode = getReturnCode();
+        encode(cb);
       }
 
       IOFSLRPCSetAttrRequest::~IOFSLRPCSetAttrRequest()
       {
          zoidfs::hints::zoidfs_hint_free(&op_hint_);
       }
-
-      size_t IOFSLRPCSetAttrRequest::rpcEncodedInputDataSize()
-      {
-          return 0;
-      }
-
-      size_t IOFSLRPCSetAttrRequest::rpcEncodedOutputDataSize()
-      {
-          return 0;
-      }
-
    }
 }
