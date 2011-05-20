4af1feb6a25f57873fd67b057e94601467798fc8
Benjamin Welton <welton@mcs.anl.gov>
2011-05-20 12:32:21 -0500

    Added null request (server).
    references #86

--- src/iofwd/rpcfrontend/IOFSLRPCNullRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCNullRequest.hh
@@ -3,41 +3,36 @@
 
 #include "zoidfs/util/zoidfs-wrapped.hh"
 #include "zoidfs/zoidfs.h"
-
+#include "encoder/EncoderStruct.hh"
+#include "encoder/EncoderString.hh"
 #include "iofwd/NullRequest.hh"
-#include "iofwd/rpcfrontend/IOFSLRPCRequest.hh"
+#include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
 #include "iofwdevent/CBType.hh"
 namespace iofwd
 {
    namespace rpcfrontend
    {
+      ENCODERSTRUCT (RPCNotImpliin, );
+      ENCODERSTRUCT (RPCNotImpliOut, ((int)(returnCode)))
       class IOFSLRPCNullRequest :
-          public IOFSLRPCRequest,
+          public RPCSimpleRequest<RPCNotImpliin, RPCNotImpliOut>,
           public NullRequest
       {
           public:
               IOFSLRPCNullRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  IOFSLRPCRequest(in, out),
+                  RPCSimpleRequest<RPCNotImpliin, RPCNotImpliOut>(in, out),
                   NullRequest(opid)
               {
               }
             
               ~IOFSLRPCNullRequest();
-
-              /* encode and decode helpers for RPC data */
-              void decode() {};
-              void encode() { process(enc_, getReturnCode()); };
-
               /* request processing */
               const ReqParam & decodeParam() {return param_;};
               void reply (const CBType & cb);
           protected:
               /* data size helpers for this request */ 
-              virtual size_t rpcEncodedInputDataSize()  { return 0; }; 
-              virtual size_t rpcEncodedOutputDataSize() { return 0; };
-
               ReqParam param_;
       };
    }
