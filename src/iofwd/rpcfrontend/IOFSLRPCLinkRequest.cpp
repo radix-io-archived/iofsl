f7f7517afd86f39799b75ea33e95511c8dfdc3d3
Benjamin Welton <welton@mcs.anl.gov>
2011-05-20 11:17:18 -0500

    Added link request for server
    references #86

--- src/iofwd/rpcfrontend/IOFSLRPCLinkRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCLinkRequest.cpp
@@ -6,43 +6,33 @@ namespace iofwd
 {
    namespace rpcfrontend
    {
-        RPC_GENPROCESS (IOFSLRPCLinkRequest, (()(from_full_path)) 
-                                        ((&)(from_parent_handle))
-                                        (()(from_component_name))              
-                                        (()(to_full_path))
-                                        ((&)(to_parent_handle))
-                                        (()(to_component_name)),
-                                        (()(returnCode))
-                                        ((*)(from_parent_hint))
-                                        ((*)(to_parent_hint)))
 
       const IOFSLRPCLinkRequest::ReqParam & IOFSLRPCLinkRequest::decodeParam() 
       { 
-          decodeRPCInput(); 
-          if (dec_struct.from_full_path.value.size() > 0)
+          if (inStruct.from_full_path.value.size() > 0)
           {
-            param_.from_full_path = const_cast<char *>(dec_struct.from_full_path.value.c_str());
+            param_.from_full_path = const_cast<char *>(inStruct.from_full_path.value.c_str());
             param_.from_component_name = 0;
             param_.from_parent_handle = 0; 
           }
           else
           {
             param_.from_full_path = 0;
-            param_.from_component_name = const_cast<char *>(dec_struct.from_component_name.value.c_str());
-            param_.from_parent_handle = &dec_struct.from_parent_handle; 
+            param_.from_component_name = const_cast<char *>(inStruct.from_component_name.value.c_str());
+            param_.from_parent_handle = &inStruct.from_parent_handle; 
           }
 
-          if (dec_struct.to_full_path.value.size() > 0)
+          if (inStruct.to_full_path.value.size() > 0)
           {
-            param_.to_full_path = const_cast<char *>(dec_struct.to_full_path.value.c_str());
+            param_.to_full_path = const_cast<char *>(inStruct.to_full_path.value.c_str());
             param_.to_component_name = 0;
             param_.to_parent_handle = 0;
           }
           else
           {
             param_.to_full_path = 0;
-            param_.to_component_name = const_cast<char *>(dec_struct.to_component_name.value.c_str());
-            param_.to_parent_handle = &dec_struct.to_parent_handle;
+            param_.to_component_name = const_cast<char *>(inStruct.to_component_name.value.c_str());
+            param_.to_parent_handle = &inStruct.to_parent_handle;
           }
 
           param_.op_hint = &op_hint_; 
@@ -59,29 +49,22 @@ namespace iofwd
           ASSERT(getReturnCode() != zoidfs::ZFS_OK || from_parent_hint_ || to_parent_hint_);
 
           /* store ptr to the attr */
-          to_parent_hint = const_cast<zoidfs::zoidfs_cache_hint_t *>(to_parent_hint_);
-          from_parent_hint = const_cast<zoidfs::zoidfs_cache_hint_t *>(from_parent_hint_);
+          outStruct.returnCode = getReturnCode();
+          outStruct.to_parent_hint = *to_parent_hint_;
+          outStruct.from_parent_hint = *from_parent_hint_;
+      
+          zoidfs::zoidfs_op_hint_t op_hint_;
+          zoidfs::hints::zoidfs_hint_create(&op_hint_);  
+          /* @TODO: Remove this later */
+          param_.op_hint = &op_hint_;
 
           /* encode */
-          encodeRPCOutput();
-
-          /* invoke the callback */
-          //cb();
+          encode(cb);          
       }
 
       IOFSLRPCLinkRequest::~IOFSLRPCLinkRequest()
       {
          zoidfs::hints::zoidfs_hint_free(&op_hint_);
-      }
-
-      size_t IOFSLRPCLinkRequest::rpcEncodedInputDataSize()
-      {
-          return 0;
-      }
-
-      size_t IOFSLRPCLinkRequest::rpcEncodedOutputDataSize()
-      {
-          return 0;
       }
 
    }
