2cf83fe5d55f0febf1ad18b23cfe41ee4418ecde
Jason Cope <copej@mcs.anl.gov>
2011-05-20 11:04:22 -0500

    added 32-bit fixes to the SM scoped ptrs

--- src/iofwd/tasksm/ReadTaskSM.cpp
+++ src/iofwd/tasksm/ReadTaskSM.cpp
@@ -55,13 +55,27 @@ namespace iofwd
     void ReadTaskSM::readNormal()
     {
 #if SIZEOF_SIZE_T == SIZEOF_INT64_T
-        api_->read (slots_[READ_SLOT], &ret_, p.handle, p.mem_count,
-              reinterpret_cast<void**>(p.mem_starts.get()), p.mem_sizes.get(),
-              p.file_count, p.file_starts.get(), p.file_sizes.get(),
-              const_cast<zoidfs::zoidfs_op_hint_t *>((*p.op_hint)()));
+        api_->read (slots_[READ_SLOT],
+                &ret_,
+                p.handle,
+                p.mem_count,
+                reinterpret_cast<void**>(p.mem_starts.get()),
+                p.mem_sizes.get(),
+                p.file_count,
+                p.file_starts.get(),
+                p.file_sizes.get(),
+                const_cast<zoidfs::zoidfs_op_hint_t *>((*p.op_hint)()));
 #else
-        api_->read(slots_[READ_SLOT], &ret_, p.handle, p.mem_count, (void**)p.mem_starts, p.mem_sizes,
-              p.file_count, p.file_starts, p.file_sizes, p.op_hint);
+        api_->read(slots_[READ_SLOT],
+                &ret_,
+                p.handle,
+                p.mem_count,
+                reinterpret_cast<void**>(p.mem_starts.get()),
+                p.mem_sizes.get(),
+                p.file_count,
+                p.file_starts.get(),
+                p.file_sizes.get(),
+                const_cast<zoidfs::zoidfs_op_hint_t *>((*p.op_hint)()));
 #endif
 
         /* set the callback */
