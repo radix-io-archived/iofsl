45847d8ae0435752b5ea1eee76fc9edf91a2d30e
Benjamin Welton <welton@mcs.anl.gov>
2011-05-26 16:14:45 -0500

    Changed Remove Request to use common encode/decode processors.

--- src/iofwd/rpcfrontend/IOFSLRPCRemoveRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCRemoveRequest.cpp
@@ -7,32 +7,39 @@ namespace iofwd
    namespace rpcfrontend
    {
       const IOFSLRPCRemoveRequest::ReqParam & IOFSLRPCRemoveRequest::decodeParam() 
-      { 
-          if (inStruct.full_path.value.size() > 0)
-          {
-            param_.full_path = const_cast<char *>(inStruct.full_path.value.c_str());
+      {
+          /* decode the rpc input params */
+         if(inStruct.info.full_path.value.c_str()[0])
+         {
+            param_.full_path = (char *)inStruct.info.full_path.value.c_str();
             param_.component_name = 0;
-            param_.parent_handle = 0; 
-          }
-          else
-          {
+            param_.parent_handle = 0;
+         }
+         else
+         {
             param_.full_path = 0;
-            param_.component_name = const_cast<char *>(inStruct.component_name.value.c_str());
-            param_.parent_handle = &inStruct.parent_handle; 
-          }
-          param_.op_hint = &op_hint_; 
-          return param_; 
+            param_.parent_handle = &inStruct.info.handle;
+            param_.component_name = (char *)inStruct.info.component.value.c_str();
+         }
+         param_.op_hint = NULL;
+
+         return param_;
       }
 
       void IOFSLRPCRemoveRequest::reply(const CBType & cb, 
                               const zoidfs::zoidfs_cache_hint_t * parent_hint_)
       {
           /* verify the args are OK */
-          ASSERT(getReturnCode() != zoidfs::ZFS_OK || parent_hint_);
+          ASSERT(getReturnCode() == zoidfs::ZFS_OK);
 
           /* store ptr to the attr */
           outStruct.parent_hint = *parent_hint_;
           outStruct.returnCode = getReturnCode();
+
+          zoidfs::hints::zoidfs_hint_create(&op_hint_);  
+          /* @TODO: Remove this later */
+          param_.op_hint = &op_hint_;
+
           encode(cb);
       }
 
--- src/iofwd/rpcfrontend/IOFSLRPCRemoveRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCRemoveRequest.hh
@@ -3,40 +3,25 @@
 
 #include "zoidfs/util/zoidfs-wrapped.hh"
 #include "zoidfs/zoidfs.h"
-#include "encoder/EncoderStruct.hh"
-#include "encoder/EncoderString.hh"
 #include "iofwd/RemoveRequest.hh"
 #include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
-#include "encoder/EncoderWrappers.hh"
+#include "common/rpc/CommonRequest.hh"
+
 using namespace encoder;
 
 namespace iofwd
 {
    namespace rpcfrontend
    {
-
-      typedef zoidfs::zoidfs_sattr_t zoidfs_sattr_t;
-      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
-      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
-      typedef zoidfs::zoidfs_cache_hint_t zoidfs_cache_hint_t;
-      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
-
-      ENCODERSTRUCT (IOFSLRPCRemoveDec, ((EncoderString)(full_path))
-                                        ((EncoderString)(component_name))
-                                        ((zoidfs_handle_t)(parent_handle)))
-
-      ENCODERSTRUCT (IOFSLRPCRemoveEnc, ((int)(returnCode))
-                                        ((zoidfs_cache_hint_t)(parent_hint)))
-
       class IOFSLRPCRemoveRequest :
-          public RPCSimpleRequest<IOFSLRPCRemoveDec, IOFSLRPCRemoveEnc>,
+          public RPCSimpleRequest<common::RPCRemoveRequest, common::RPCRemoveResponse>,
           public RemoveRequest
       {
           public:
               IOFSLRPCRemoveRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  RPCSimpleRequest<IOFSLRPCRemoveDec, IOFSLRPCRemoveEnc>(in, out),
+                  RPCSimpleRequest<common::RPCRemoveRequest, common::RPCRemoveResponse>(in, out),
                   RemoveRequest(opid)
               {
               }
