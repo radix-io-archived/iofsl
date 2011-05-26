8521a61af2f2c81c4d0079521b9160840b26c045
Benjamin Welton <welton@mcs.anl.gov>
2011-05-26 15:48:12 -0500

    Changed Mkdir request on server to use common processor structs

--- src/iofwd/rpcfrontend/IOFSLRPCMkdirRequest.cpp
+++ src/iofwd/rpcfrontend/IOFSLRPCMkdirRequest.cpp
@@ -9,20 +9,20 @@ namespace iofwd
 
       const IOFSLRPCMkdirRequest::ReqParam & IOFSLRPCMkdirRequest::decodeParam() 
       { 
-          if (inStruct.full_path.value.size() > 0)
+          if (inStruct.dir.full_path.value.size() > 0)
           {
-            param_.full_path = const_cast<char *>(inStruct.full_path.value.c_str());
+            param_.full_path = const_cast<char *>(inStruct.dir.full_path.value.c_str());
             param_.component_name = 0;
             param_.parent_handle = 0; 
           }
           else
           {
             param_.full_path = 0;
-            param_.component_name = const_cast<char *>(inStruct.component_name.value.c_str());
-            param_.parent_handle = &inStruct.parent_handle; 
+            param_.component_name = const_cast<char *>(inStruct.dir.component.value.c_str());
+            param_.parent_handle = &inStruct.dir.handle; 
           }
           param_.sattr = &inStruct.sattr;
-          param_.op_hint = &op_hint_; 
+          param_.op_hint = NULL; 
           return param_; 
       }
 
@@ -30,11 +30,15 @@ namespace iofwd
                               const zoidfs::zoidfs_cache_hint_t * parent_hint_)
       {
           /* verify the args are OK */
-          ASSERT(getReturnCode() != zoidfs::ZFS_OK || parent_hint_);
+          ASSERT(getReturnCode() == zoidfs::ZFS_OK);
 
           /* store ptr to the attr */
           outStruct.parent_hint = *parent_hint_;
           outStruct.returnCode = getReturnCode();
+  
+          zoidfs::hints::zoidfs_hint_create(&op_hint_);  
+          /* @TODO: Remove this later */
+          param_.op_hint = &op_hint_;
 
           encode(cb);
       }
