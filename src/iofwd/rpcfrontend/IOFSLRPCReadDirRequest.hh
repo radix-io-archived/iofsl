939a66948fc7c7ac8a95678e63f5b3d0af975b23
Benjamin Welton <welton@mcs.anl.gov>
2011-05-26 15:48:45 -0500

    Changed ReadDir (server) to use common processor functions

--- src/iofwd/rpcfrontend/IOFSLRPCReadDirRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCReadDirRequest.hh
@@ -7,36 +7,22 @@
 #include "iofwd/ReadDirRequest.hh"
 #include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
 
+#include "common/rpc/CommonRequest.hh"
+
 namespace iofwd
 {
    namespace rpcfrontend
    {
-      typedef zoidfs::zoidfs_dirent_t zoidfs_dirent_t;
-      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
-      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
-      typedef zoidfs::zoidfs_dirent_cookie_t zoidfs_dirent_cookie_t;
-      typedef zoidfs::zoidfs_cache_hint_t zoidfs_cache_hint_t;
-      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
-
-      ENCODERSTRUCT (IOFSLRPCReadDirDec, ((zoidfs_handle_t)(handle)) 
-                                        ((zoidfs_dirent_cookie_t)(cookie))
-                                        ((uint32_t)(entry_count))              
-                                        ((uint32_t)(flags)))
-
-      ENCODERSTRUCT (IOFSLRPCReadDirEnc, ((int)(returnCode))
-                                         ((uint32_t)(entry_count))
-                                         ((zoidfs_dirent_t)(entries))
-                                         ((zoidfs_cache_hint_t)(cache)))
 
       class IOFSLRPCReadDirRequest :
-          public RPCSimpleRequest<IOFSLRPCReadDirDec, IOFSLRPCReadDirEnc>,
+          public RPCSimpleRequest<common::RPCReadDirRequest, common::RPCReadDirResponse>,
           public ReadDirRequest
       {
           public:
               IOFSLRPCReadDirRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  RPCSimpleRequest<IOFSLRPCReadDirDec, IOFSLRPCReadDirEnc>(in, out),
+                  RPCSimpleRequest<common::RPCReadDirRequest, common::RPCReadDirResponse>(in, out),
                   ReadDirRequest(opid)
               {
               }
