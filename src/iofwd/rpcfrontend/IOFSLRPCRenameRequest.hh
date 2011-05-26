d3a883f4592f9bb5e5830bdd8f95f90f228a4eba
Benjamin Welton <welton@mcs.anl.gov>
2011-05-26 16:20:39 -0500

    Changed server Rename request to use common encoder/decoder processors

--- src/iofwd/rpcfrontend/IOFSLRPCRenameRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCRenameRequest.hh
@@ -3,44 +3,23 @@
 
 #include "zoidfs/util/zoidfs-wrapped.hh"
 #include "zoidfs/zoidfs.h"
-#include "encoder/EncoderStruct.hh"
-#include "encoder/EncoderString.hh"
 #include "iofwd/RenameRequest.hh"
 #include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
-#include "encoder/EncoderWrappers.hh"
-using namespace encoder;
+#include "common/rpc/CommonRequest.hh"
 
 namespace iofwd
 {
    namespace rpcfrontend
    {
-
-      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
-      typedef zoidfs::zoidfs_sattr_t zoidfs_sattr_t;
-      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
-      typedef zoidfs::zoidfs_cache_hint_t zoidfs_cache_hint_t;
-
-      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
-      ENCODERSTRUCT (IOFSLRPCRenameDec, ((EncoderString)(from_full_path)) 
-                                        ((zoidfs_handle_t)(from_parent_handle))
-                                        ((EncoderString)(from_component_name))              
-                                        ((EncoderString)(to_full_path))
-                                        ((zoidfs_handle_t)(to_parent_handle))
-                                        ((EncoderString)(to_component_name)))
-
-      ENCODERSTRUCT (IOFSLRPCRenameEnc, ((int)(returnCode))
-                                        ((zoidfs_cache_hint_t)(from_parent_hint))
-                                        ((zoidfs_cache_hint_t)(to_parent_hint)))
-
       class IOFSLRPCRenameRequest :
-          public RPCSimpleRequest<IOFSLRPCRenameDec, IOFSLRPCRenameEnc>,
+          public RPCSimpleRequest<common::RPCRenameRequest, common::RPCRenameResponse>,
           public RenameRequest
       {
           public:
               IOFSLRPCRenameRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  RPCSimpleRequest<IOFSLRPCRenameDec, IOFSLRPCRenameEnc>(in, out),
+                  RPCSimpleRequest<common::RPCRenameRequest, common::RPCRenameResponse>(in, out),
                   RenameRequest(opid)
               {
               }
