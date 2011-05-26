47309b85d26090d2c8a0d957aaafb7bfe916b799
Benjamin Welton <welton@mcs.anl.gov>
2011-05-26 16:08:52 -0500

    Changed Server ReadLink to use common encode/decode framework

--- src/iofwd/rpcfrontend/IOFSLRPCReadLinkRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCReadLinkRequest.hh
@@ -6,29 +6,24 @@
 
 #include "iofwd/ReadLinkRequest.hh"
 #include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
+
+#include "encoder/EncoderString.hh"
+#include "common/rpc/CommonRequest.hh"
 namespace iofwd
 {
    namespace rpcfrontend
    {
-      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
-      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
-      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
-      ENCODERSTRUCT (IOFSLRPCReadLinkDec, ((zoidfs_handle_t)(handle))
-                                          ((size_t)(buffer_length)))
-
-      ENCODERSTRUCT (IOFSLRPCReadLinkEnc, ((int)(returnCode))
-                                          ((EncoderString)(buffer))
-                                          ((size_t)(buffer_length)))
+      //typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
 
       class IOFSLRPCReadLinkRequest :
-          public RPCSimpleRequest<IOFSLRPCReadLinkDec, IOFSLRPCReadLinkEnc>,
+          public RPCSimpleRequest<common::RPCReadLinkRequest, common::RPCReadLinkResponse>,
           public ReadLinkRequest
       {
           public:
               IOFSLRPCReadLinkRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  RPCSimpleRequest<IOFSLRPCReadLinkDec, IOFSLRPCReadLinkEnc>(in, out),
+                  RPCSimpleRequest<common::RPCReadLinkRequest, common::RPCReadLinkResponse>(in, out),
                   ReadLinkRequest(opid)
               {
               }
