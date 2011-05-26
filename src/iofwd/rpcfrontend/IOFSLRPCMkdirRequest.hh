bbb032f2c24254b8725c5a2a53d69d0e9ef5cb0b
Benjamin Welton <welton@mcs.anl.gov>
2011-05-26 15:48:12 -0500

    Changed Mkdir request on server to use common processor structs

--- src/iofwd/rpcfrontend/IOFSLRPCMkdirRequest.hh
+++ src/iofwd/rpcfrontend/IOFSLRPCMkdirRequest.hh
@@ -6,34 +6,22 @@
 
 #include "iofwd/MkdirRequest.hh"
 #include "iofwd/rpcfrontend/RPCSimpleRequest.hh"
+
+#include "common/rpc/CommonRequest.hh"
+
 namespace iofwd
 {
    namespace rpcfrontend
    {
-
-      typedef zoidfs::zoidfs_sattr_t zoidfs_sattr_t;
-      typedef zoidfs::zoidfs_op_hint_t zoidfs_op_hint_t;
-      typedef zoidfs::zoidfs_handle_t zoidfs_handle_t;
-      typedef zoidfs::zoidfs_cache_hint_t zoidfs_cache_hint_t;
-      typedef encoder::EncoderString<0, ZOIDFS_PATH_MAX> EncoderString;
-      ENCODERSTRUCT (IOFSLRPCMkdirDec, ((EncoderString)(full_path)) 
-                                        ((zoidfs_handle_t)(parent_handle))
-                                        ((EncoderString)(component_name))              
-                                        ((zoidfs_sattr_t)(sattr)))
-
-      ENCODERSTRUCT (IOFSLRPCMkdirEnc, ((int)(returnCode))
-                                       ((zoidfs_cache_hint_t)(parent_hint)))
-
-
       class IOFSLRPCMkdirRequest :
-          public RPCSimpleRequest<IOFSLRPCMkdirDec, IOFSLRPCMkdirEnc>,
+          public RPCSimpleRequest<common::RPCMkdirRequest, common::RPCMkdirResponse>,
           public MkdirRequest
       {
           public:
               IOFSLRPCMkdirRequest(int opid,
                       iofwdevent::ZeroCopyInputStream * in,
                       iofwdevent::ZeroCopyOutputStream * out) :
-                  RPCSimpleRequest<IOFSLRPCMkdirDec, IOFSLRPCMkdirEnc>(in, out),
+                  RPCSimpleRequest<common::RPCMkdirRequest, common::RPCMkdirResponse>(in, out),
                   MkdirRequest(opid)
               {
               }
