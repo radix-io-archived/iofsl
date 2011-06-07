2de9171f35ccfcc5a25a969b685712f3621d3784
Rico D'Amore <rjdamore@gmail.com>
2011-06-07 15:12:21 -0500

    Fixed email issue with email_results.txt

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -3,6 +3,7 @@
 #
 # Prepares, builds and tests 
 #
+IOFWD_SRCDIR=/homes/rjdamore/iofsl
 SRCDIR=$PWD
 if test -z "${IOFWD_SRCDIR}" ; then
    echo "Need IOFWD_SRCDIR ! (to find configoptions)"
@@ -12,7 +13,7 @@ fi
 
 SCRIPTDIR="${IOFWD_SRCDIR}/scripts"
 CONFIGFILE="${SCRIPTDIR}/configoptions.$(hostname)"
-if test -r "${CONFIGFILE}" ; echo "config file is $CONFIGFILE" ; then
+if test -r "${CONFIGFILE}" ; echo "config file is $CONFIGFILE.$(hostname)" ; then
    source  "${CONFIGFILE}" ; echo "configure options = $CONFIGURE_OPTIONS" 
 else echo "could not use config file"
 fi
