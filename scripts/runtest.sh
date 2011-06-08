17e9fe10b2fc0e72edc30dce8b58883f18847151
Rico D'Amore <rjdamore@gmail.com>
2011-06-08 11:01:44 -0500

    removed redundant configfile name

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -13,7 +13,7 @@ fi
 
 SCRIPTDIR="${IOFWD_SRCDIR}/scripts"
 CONFIGFILE="${SCRIPTDIR}/configoptions.$(hostname)"
-if test -r "${CONFIGFILE}" ; echo "config file is $CONFIGFILE.$(hostname)" ; then
+if test -r "${CONFIGFILE}" ; echo "config file is $CONFIGFILE" ; then
    source  "${CONFIGFILE}" ; echo "configure options = $CONFIGURE_OPTIONS" 
 else echo "could not use config file"
 fi
