a533982cd4aae4362d2c14a2d517fdfa516ea7b7
Dries Kimpe <dkimpe@mcs.anl.gov>
2011-09-12 11:01:52 -0500

    Add fwd header for hash library

---
+++ src/iofwdutil/hash/hash-fwd.hh
@@ -0,0 +1,17 @@
+#ifndef IOFWDUTIL_HASH_HASH_FWD_HH
+#define IOFWDUTIL_HASH_HASH_FWD_HH
+
+namespace iofwdutil
+{
+   namespace hash
+   {
+      //=====================================================================
+
+      class HashFunc;
+
+      //=====================================================================
+   }
+}
+
+
+#endif
