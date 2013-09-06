49bc7534bc4195ce4f9d022caf2b048224ef1cd2
Dries Kimpe <dkimpe@mcs.anl.gov>
2013-09-06 11:11:38 -0500

    Working on nightly builds

--- autobuild/build.sh
+++ autobuild/build.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 # Build script
+ls ${WORKSPACE}
+
+AUTOBUILDDIR=${WORKSPACE}/autobuild
 
 # Dump some information
 echo "======================================================================="
 echo "= Build ${BUILD_NUMBER} ${BUILD_ID}"
-echo "     on ${NODE_NAME} [${NODE_LABELS}]"
+echo "=    on ${NODE_NAME} [${NODE_LABELS}]"
 echo "======================================================================="
 
+echo
+echo "======================================================================="
+echo "==== STEP 1: Fetch build dependencies ================================="
+echo "======================================================================="
 
 
 
