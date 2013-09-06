dc72ac8cb024a38db7ff86a46ca1386756c821e3
Dries Kimpe <dkimpe@mcs.anl.gov>
2013-09-06 11:04:46 -0500

    Work on autobuild infrastructure

--- autobuild/build.sh
+++ autobuild/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
 # Build script
-#
 
+# Dump some information
+echo "======================================================================="
+echo "= Build ${BUILD_NUMBER} ${BUILD_ID}"
+echo "     on ${NODE_NAME} [${NODE_LABELS}]"
+echo "======================================================================="
+
+
+
+
+
+# force failure for now
+exit 1
